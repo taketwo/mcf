bc35f85d866c1adac411d23d9337f4ce06241756
Sergey Alexandrov <alexandrov88@gmail.com>
2012-08-23 22:35:22 +0200

    Added support for ros dynamic_reconfigure files.

--- scripts/ros-create-dotfiles.py
+++ scripts/ros-create-dotfiles.py
@@ -23,6 +23,7 @@ def create_vim(pack):
     vim.write('let makeprg="rosmake\ %s"\n' % pack)
     vim.write('au FileType cpp :UltiSnipsAddFiletypes ros.cpp\n')
     vim.write('au BufRead,BufNewFile */manifest.xml :UltiSnipsAddFiletypes rosmanifest\n')
+    vim.write('au BufRead,BufNewFile *.cfg set filetype=python | :UltiSnipsAddFiletypes roscfg.python')
     vim.close()
 
 
