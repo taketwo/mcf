63e7a359a3a9edc12924ac89d6a60a533c723f86
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-23 14:19:26 +0200

    Updated mcf installation suite and moved it to scripts/ folder.

---
+++ scripts/install-mcf.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+
+files = [('.Xmodmap', 'Remap CapsLock to Escape'),
+         ('.dircolors', 'Dircolors for Solarized palette'),
+         ('.fonts', 'Custom fonts'),
+         ('.vim', 'VIM settings/plugins folder'),
+         ('.vimrc', 'VIM configuration file'),
+         ('.bashrc', 'Bash configuration file'),
+         ('.gitconfig', 'GIT configuration file')]
+
+pypi_packages = ['flake8', 'pep8']
+
+
+def link(filename, desc):
+    home = os.path.expanduser('~')
+    src = os.path.join('.mcf', filename)
+    dest = os.path.join(home, filename)
+    if os.path.lexists(dest):
+        os.unlink(dest)
+    os.symlink(src, dest)
+    print '[*]', desc
+    print '   ', dest, '->', src
+
+
+def pypi(package):
+    print '[*]', package
+    cmd = 'sudo pip install --upgrade %s' % package
+    subprocess.call(cmd.split())
+
+
+if __name__ == '__main__':
+    home = os.path.expanduser('~')
+    mcf = os.path.join(home, '.mcf')
+
+    print 'MCF install script.'
+    print ''
+
+    print 'Creating symlinks...'
+    print ''
+    for f, d in files:
+        link(f, d)
+    print ''
+
+    print 'Installing python packages...'
+    print ''
+    for p in pypi_packages:
+        pypi(p)
+    print ''
+
+    print 'Configuring gnome terminal for Solarized palette...'
+    print ''
+    subprocess.call(os.path.join(mcf, 'scripts',
+                                 'install-solarized-colors.bash'))
+
+    print 'Setting wallpaper...'
+    print ''
+    uri = 'file://' + os.path.join(mcf, 'wallpapers', 'stabilis.jpg')
+    subprocess.call(('gsettings set org.gnome.desktop.background picture-uri' +
+                     ' ' + uri).split())
+    print ''
+    print 'Installation completed.'
+    print 'You will need to install clang manually.'
+
+# Clang installation.
+# 1) Follow instructions at http://clang.llvm.org/get_started.html
+# 2) Configure with options:
+#    ../configure --enable-optimized --disable-assertions --prefix=/opt/llvm
