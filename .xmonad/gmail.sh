b7bd1da6be6f96fd101e8790df7215eb90e6900b
Sergey Alexandrov <alexandrov88@gmail.com>
2013-05-30 00:33:04 +0200

    Updated mail indicator.

--- .xmonad/gmail.sh
+++ .xmonad/gmail.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 user="alexandrov88"
 pass=""
 new=`curl -u $user:$pass -s -sslreqd "https://mail.google.com/mail/feed/atom" | grep -c "<entry>"`
-echo $new
+
+echo -n "^fg(\#a9a6af)^i(/home/sergey/.xmonad/icons/mail.xbm)^fg() "
+if [[ $new > 0 ]]; then
+  echo -n "^fg(\#ff7272)$new^fg()"
+else
+  echo -n "$new"
+fi
