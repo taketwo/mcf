57325cb15d8d078d4196ae715e6f0f2acff9b5d0
Sergey Alexandrov <alexandrov88@gmail.com>
2013-05-30 15:56:04 +0200

    Changed the colors in gmail indicator.

--- .xmonad/gmail.sh
+++ .xmonad/gmail.sh
@@ -2,11 +2,12 @@
 
 user="alexandrov88"
 pass=""
-new=`curl -u $user:$pass -s -sslreqd "https://mail.google.com/mail/feed/atom" | grep -c "<entry>"`
+count=`curl -u $user:$pass -s -sslreqd "https://mail.google.com/mail/feed/atom" | grep -c "<entry>"`
 
-echo -n "^fg(\#a9a6af)^i(/home/sergey/.xmonad/icons/mail.xbm)^fg() "
-if [[ $new > 0 ]]; then
-  echo -n "^fg(\#ff7272)$new^fg()"
+mail="^i(/home/sergey/.xmonad/icons/mail.xbm)"
+
+if [[ $count > 0 ]]; then
+  echo -n "^fg(#ff7272)$mail^fg() $count"
 else
-  echo -n "$new"
+  echo -n "$mail $count"
 fi
