0b65f2c8dab1ac1c25b0c2970c1cb1acd7b2a50e
Sergey Alexandrov <alexandrov88@gmail.com>
2013-05-30 12:10:40 +0200

    Added a script to install xcape from source.

---
+++ scripts/install/install-xcape.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import argparse
+import subprocess
+
+from install import GitRepository, make_install
+
+
+def replace(e, f):
+    cmd = ['sed', '-i', '-e', e, f]
+    subprocess.call(cmd)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+    Install xcape from source.
+
+    Steps involved:
+     - clone github repository into a temp folder
+     - make install (will go to /usr/local/)
+     - remove temp folder
+    ''', formatter_class=argparse.RawDescriptionHelpFormatter)
+    args = parser.parse_args()
+
+    with GitRepository('alols/xcape') as repo:
+        # Change installation path to /usr/local
+        replace('s/\/usr/\/usr\/local/', 'Makefile')
+        # Make and install
+        make_install()
