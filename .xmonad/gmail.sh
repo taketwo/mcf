fac2858c376b64fa85b58af3c12ed2ed462d60d5
Sergey Alexandrov <alexandrov88@gmail.com>
2013-06-03 00:24:06 +0200

    Improved mail indicator.

--- .xmonad/gmail.sh
+++ .xmonad/gmail.sh
@@ -4,10 +4,13 @@ user="alexandrov88"
 pass=""
 count=`curl -u $user:$pass -s -sslreqd -m 5 "https://mail.google.com/mail/feed/atom" | grep -c "<entry>"`
 
+solarizedRed="#dc322f"
+solarizedBase3="#fdf6e3"
+
 mail="^i(/home/sergey/.xmonad/icons/mail.xbm)"
 
 if [[ $count > 0 ]]; then
-  echo -n "^fg(#ff7272)$mail^fg() $count"
+  echo -n "^bg($solarizedRed)^fg($solarizedBase3) $mail $count ^fg()^bg()"
 else
-  echo -n "$mail $count"
+  echo -n " $mail $count "
 fi
