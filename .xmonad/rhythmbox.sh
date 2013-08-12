ff9a14b42e609726bd61945fa4a3287040df358a
Sergey Alexandrov <alexandrov88@gmail.com>
2013-08-12 21:21:15 +0200

    Updated rhythmbox.sh to show [paused] when Rhythmbox is running, but stopped.

--- .xmonad/rhythmbox.sh
+++ .xmonad/rhythmbox.sh
@@ -2,9 +2,12 @@
 
 rhythmbox=`/usr/bin/rhythmbox-client --print-playing --no-start --no-present`
 
-if [[ $rhythmbox = " - XFM London" ]]; then
-  xfm=`curl -s -m 5 "http://rope.ixfm.fimc.net/Feeds/NowPlaying/GCap_Media/XFM_London/5893.xml" | tr '\n' ' ' | sed 's/.*title="\([^"]*\).*name="\([^"]*\).*/\2 - \1/'`
-  echo -n " $xfm"
-else
-  echo -n " $rhythmbox"
-fi
+case $rhythmbox in
+  " - XFM London" )
+    xfm=`curl -s -m 5 "http://rope.ixfm.fimc.net/Feeds/NowPlaying/GCap_Media/XFM_London/5893.xml" | tr '\n' ' ' | sed 's/.*title="\([^"]*\).*name="\([^"]*\).*/\2 - \1/'`
+    echo -n " $xfm";;
+  " - " )
+    echo -n " [paused]";;
+  *)
+    echo -n " $rhythmbox";;
+esac
