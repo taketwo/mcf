0f2715f49494e2471b89d8e454d5fffccf9bd0e9
Sergey Alexandrov <alexandrov88@gmail.com>
2014-01-13 12:46:10 +0100

    Changed defaut terminal font to Fantasque Sans Mono

--- scripts/install/setup-terminal.bash
+++ scripts/install/setup-terminal.bash
@@ -6,6 +6,6 @@ bash $HOME/.mcf/misc/gnome-terminal-colors-solarized/install.sh -s dark -p Defau
 
 # Set custom font
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_system_font" --type boolean "false"
-gconftool-2 --set "/apps/gnome-terminal/profiles/Default/font" --type string "MenloForPowerline 11"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/font" --type string "FantasqueSansMono 11"
 
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/scrollback_lines" --type integer "1024"
