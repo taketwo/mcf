8f09426521351452c51a5b320d11f062b1866284
Sergey Alexandrov <alexandrov88@gmail.com>
2014-01-13 12:44:51 +0100

    Added installer for Fantasque Sans Mono font

---
+++ scripts/install/install-font-fantasque-sans-mono.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import argparse
+import subprocess
+
+from install import GitRepository, deb
+
+
+def replace(e, f):
+    cmd = ['sed', '-i', '-e', e, f]
+    subprocess.call(cmd)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''
+    Install Fantasque Sans Mono font from source.
+
+    Steps involved:
+     - clone github repository into a temp folder
+     - make install
+     - remove temp folder
+    ''', formatter_class=argparse.RawDescriptionHelpFormatter)
+    args = parser.parse_args()
+
+    deb(['fontforge', 'ttfautohint', 'woff-tools'])
+
+    with GitRepository('belluzj/fantasque-sans') as repo:
+        # Completely remove line with ttf2eot command
+        replace('s/^\tttf2eot.*$//', 'Makefile')
+        # Make install
+        subprocess.call(['make', 'install'])
