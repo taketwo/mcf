36968ef50ca142159b8994ea22a7ae171027d803
Sergey Alexandrov <alexandrov88@gmail.com>
2014-03-21 09:33:09 +0100

    Add several user key bindings for fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,4 @@ function fish_prompt
   ~/.mcf/scripts/bundle/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
   echo " "
 end
+
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,6 @@
+bind \e- end-of-line
+bind \e_ beginning-of-line
+bind \en forward-char
+bind \eh backward-char
+bind \ec history-search-backward
+bind \et history-search-forward
