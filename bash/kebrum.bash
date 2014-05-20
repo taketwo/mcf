297d996cb8085d55e43e004524755cdb94b48f47
Sergey Alexandrov <alexandrov88@gmail.com>
2014-05-20 12:33:14 +0200

    Partially updated kebrum.bash
    Stopping openvpn daemon needs to be fixed for Arch

--- bash/kebrum.bash
+++ bash/kebrum.bash
@@ -1,11 +1,19 @@
 function kebrum ()
 {
   case $1 in
-    off ) sudo service openvpn stop
+    off ) if hash systemctl 2>/dev/null; then
+            sudo systemctl stop openvpn
+          else
+            sudo service openvpn stop
+          fi
           ;;
     *   ) config="$1.Kebrum.TCP"
           if [ -f "/etc/openvpn/$config.conf" ]; then
-            sudo service openvpn start $config
+            if hash systemctl 2>/dev/null; then
+              sudo systemctl start openvpn@$config
+            else
+              sudo service openvpn start $config
+            fi
           fi
   esac
 }
