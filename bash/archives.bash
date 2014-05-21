ca5ea00105345dd25bebbbb7107fa8b586c1be2c
Sergey Alexandrov <alexandrov88@gmail.com>
2014-05-21 13:10:28 +0200

    Add a possibility to extract *.deb archives

--- bash/archives.bash
+++ bash/archives.bash
@@ -3,17 +3,18 @@ extract ()
 {
   if [ -f $1 ] ; then
     case $1 in
-      *.tar.bz2)   tar xvjf $1    ;;
-      *.tar.gz)    tar xvzf $1    ;;
-      *.bz2)       bunzip2 $1     ;;
-      *.rar)       rar x $1       ;;
-      *.gz)        gunzip $1      ;;
-      *.tar)       tar xvf $1     ;;
-      *.tbz2)      tar xvjf $1    ;;
-      *.tgz)       tar xvzf $1    ;;
-      *.zip)       unzip $1       ;;
-      *.Z)         uncompress $1  ;;
-      *.7z)        7z x $1        ;;
+      *.tar.bz2)   tar xvjf $1                               ;;
+      *.tar.gz)    tar xvzf $1                               ;;
+      *.bz2)       bunzip2 $1                                ;;
+      *.rar)       rar x $1                                  ;;
+      *.gz)        gunzip $1                                 ;;
+      *.tar)       tar xvf $1                                ;;
+      *.tbz2)      tar xvjf $1                               ;;
+      *.tgz)       tar xvzf $1                               ;;
+      *.zip)       unzip $1                                  ;;
+      *.Z)         uncompress $1                             ;;
+      *.7z)        7z x $1                                   ;;
+      *.deb)       ar p $1 data.tar.gz | tar zx              ;;
       *)           echo "Do not know how to extract '$1'..." ;;
     esac
   else
