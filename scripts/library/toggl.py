aeb92ba571ae8c3a2dfbc59181a5520e5b3ecb93
Sergey Alexandrov <alexandrov88@gmail.com>
2014-08-27 18:18:50 +0200

    Add rudimentary toggl.com CLI

---
+++ scripts/library/toggl.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import requests
+
+
+class Toggl:
+
+    BASE_URI = 'https://www.toggl.com/api/v8/'
+
+    def __init__(self, api_token):
+        self.auth = (api_token, 'api_token')
+
+    def get(self, action):
+        return requests.get(Toggl.BASE_URI + action, auth=self.auth)
+
+    def time_entries(self):
+        return self.get('time_entries').json()
+
+    def current(self):
+        return self.get('time_entries/current').json()['data']
