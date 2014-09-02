aac642fa819379e6176ad3a325fb501e6b849582
Sergey Alexandrov <alexandrov88@gmail.com>
2014-09-02 09:58:21 +0200

    Set unlimited scrolling in terminal configuration

--- scripts/install/setup-terminal.bash
+++ scripts/install/setup-terminal.bash
@@ -8,4 +8,6 @@ bash $HOME/.mcf/misc/gnome-terminal-colors-solarized/install.sh -s dark -p Defau
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_system_font" --type boolean "false"
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/font" --type string "FantasqueSansMono 11"
 
+# Set 1024 lines (just in case) and unlimited scrolling
 gconftool-2 --set "/apps/gnome-terminal/profiles/Default/scrollback_lines" --type integer "1024"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/scrollback_unlimited" --type bool "true"
