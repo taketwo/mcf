a3370c662a1e6779f39a319d718582f82d48d8f6
Sergey Alexandrov <alexandrov88@gmail.com>
2014-09-08 11:01:25 +0200

    Add popup mode to gmail indicator

--- .xmonad/gmail.sh
+++ .xmonad/gmail.sh
@@ -4,16 +4,39 @@ source $MCF/.xmonad/panel.bash
 
 user="alexandrov88"
 pass=`$MCF/scripts/bin/keyring-password liamg`
-count=`curl -u $user:$pass -s -sslreqd -m 5 "https://mail.google.com/mail/feed/atom" | grep -o "<entry>" | wc -l`
+feed=`curl -u $user:$pass -s -sslreqd -m 5 "https://mail.google.com/mail/feed/atom"`
 
-Start
-
-Icon "mail"
-Add $count
-
-if [[ $count > 0 ]]; then
-  Fg $SolarizedBase3
-  Bg $SolarizedRed
+if [ "$1" != "-p" ]; then
+#######################################################################
+#                           Indicator mode                            #
+#######################################################################
+  count=`echo "$feed" | grep -o "<entry>" | wc -l`
+  Start
+  Icon "mail"
+  Add $count
+  if [[ $count > 0 ]]; then
+    Fg $SolarizedBase3
+    Bg $SolarizedRed
+  fi
+  Flush
+else
+#######################################################################
+#                             Popup mode                              #
+#######################################################################
+  # add protection from multiple instances (test ps aux | grep ...)
+  width=400
+  font_pixelsize=11
+  line_height=$((font_pixelsize + 4))
+  lines=8
+  height=$((line_height * (lines + 1)))
+  bottom_gap=20
+  read _ s_width <<< "$(xwininfo -root | egrep Width)"
+  read _ s_height <<< "$(xwininfo -name "dzen title" | egrep Height)"
+  {
+  echo '^fg(#a00)GMail^fg()'
+  titles=`echo "$feed" | grep -oP "(?<=<entry><title>)(.*?)(?=</title>)"`
+  echo $titles
+  echo '^uncollapse()'
+  } | dzen2 -x $((s_width - width)) -y $s_height -w $width -l $lines -h $line_height -sa center -fn "Liberation Mono:pixelsize=$font_pixelsize" -e 'leaveslave=exit;button1=exit;button3=exit;onstart=uncollapse' -p -bg "#3c3b37" -title-name 'gmail_popup'
 fi
 
-Flush
