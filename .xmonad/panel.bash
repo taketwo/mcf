1e0311b56a1cfd8f6b5dcbdb99b9476aaece7929
Sergey Alexandrov <alexandrov88@gmail.com>
2014-09-09 15:31:03 +0200

    Add NewLine, StartPopup, and FlushPopup functions

--- .xmonad/panel.bash
+++ .xmonad/panel.bash
@@ -1,19 +1,19 @@
 StartIndicator ()
 {
-  eval "output=' '"
+  eval "__output=' '"
 }
 
 Fg ()
 {
-  if [ "$output" != " " ] ; then
-    eval "output='^fg($1)$output^fg()'"
+  if [ "$__output" != " " ] ; then
+    eval "__output='^fg($1)$__output^fg()'"
   fi
 }
 
 Bg ()
 {
-  if [ "$output" != " " ] ; then
-    eval "output='^bg($1)$output^bg()'"
+  if [ "$__output" != " " ] ; then
+    eval "__output='^bg($1)$__output^bg()'"
   fi
 }
 
@@ -25,26 +25,60 @@ Bg ()
 #   Click 1 "keyboard -n"
 Click ()
 {
-  if [ "$output" != " " ] ; then
-    eval "output='^ca($1, $2)$output^ca()'"
+  if [ "$__output" != " " ] ; then
+    eval "__output='^ca($1, $2)$__output^ca()'"
   fi
 }
 
 Icon ()
 {
-  eval "output='$output^i(/home/sergey/.xmonad/icons/${1}.xbm) '"
+  eval "__output='$__output^i(/home/sergey/.xmonad/icons/${1}.xbm) '"
 }
 
 Add ()
 {
   # Escape single quote with triple hat
-  eval "output='$output${1//\'/^^^} '"
+  eval "__output='$__output${1//\'/^^^} '"
 }
 
 FlushIndicator ()
 {
-  if [ "$output" != " " ] ; then
+  if [ "$__output" != " " ] ; then
     # Undo triple hat escape
-    echo -n "${output//^^^/\'}"
+    echo -n "${__output//^^^/\'}"
   fi
 }
+
+NewLine ()
+{
+  eval '__popup=("${__popup[@]}" "$__output")'
+  eval '__output=""'
+}
+
+StartPopup ()
+{
+  eval "__output=''"
+  eval "__popup=('')"
+}
+
+FlushPopup ()
+{
+  if [ "$__popup" != " " ] ; then
+    width=480
+    font_pixelsize=11
+    line_height=$((font_pixelsize + 4))
+    lines=${#__popup[@]}
+    height=$((line_height * (lines + 1)))
+    bottom_gap=20
+    read _ s_width <<< "$(xwininfo -root | egrep Width)"
+    read _ s_height <<< "$(xwininfo -name "dzen title" | egrep Height)"
+   {
+      for line in "${__popup[@]}"; do
+        # Undo triple hat escape
+        echo "${line//^^^/\'}"
+      done
+      echo '^uncollapse()'
+   } | dzen2 -x $((s_width - width)) -y $s_height -w $width -l $lines -h $line_height -sa left -fn "Liberation Mono:pixelsize=$font_pixelsize" -e 'leaveslave=exit;button1=exit;button3=exit;onstart=uncollapse' -p -bg "#3c3b37" -title-name 'gmail_popup'
+  fi
+}
+
