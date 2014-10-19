29bc97fea72b0e80607375268a706509538e7960
Sergey Alexandrov <alexandrov88@gmail.com>
2014-10-20 01:35:47 +0200

    Update kebrum installation script

--- scripts/install/install-kebrum.bash
+++ scripts/install/install-kebrum.bash
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-source /home/sergey/.mcf/bash/archives.bash
+DEST="/etc/openvpn"
 
+# Remove previous istallation
+cd $DEST
+rm *Kebrum*conf
+rm -rf ssl/{ta.key,ca.crt,client.crt,client.key}
+
+# Download and install new files
 mkdir /tmp/kebrum
 cd /tmp/kebrum
 wget https://en.kebrum.com/webclient/config.zip
-extract config.zip
-mv ssl /etc/openvpn/
+unzip config.zip
+mv ssl $DEST
 for config in *TCP*; do
   echo "auth-user-pass /home/sergey/.kebrum" >> "$config"
   IFS='.' read -ra tokens <<< "$config"
   name="${tokens// /}.Kebrum.TCP.conf"
-  mv "$config" /etc/openvpn/$name
+  mv "$config" $DEST/$name
 done
 cd ..
 rm -rf kebrum
