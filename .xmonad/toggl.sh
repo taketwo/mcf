98722845f7a3986ec0da221b4c45cbd8b2546357
Sergey Alexandrov <alexandrov88@gmail.com>
2014-12-07 13:37:39 +0100

    Add popup mode for toggl toolbar icon

--- .xmonad/toggl.sh
+++ .xmonad/toggl.sh
@@ -3,12 +3,26 @@
 source $MCF/.xmonad/panel.bash
 source $MCF/.xmonad/solarized.bash
 
-StartIndicator
-
-if [[ `$MCF/scripts/bin/toggl running` == "yes" ]]; then
-  Icon "toggl"
-  Bg $SolarizedGreen
+if [ "$1" != "-p" ]; then
+#######################################################################
+#                           Indicator mode                            #
+#######################################################################
+  StartIndicator
+  if [[ `$MCF/scripts/bin/toggl running` == "yes" ]]; then
+    Icon "toggl"
+    Bg $SolarizedGreen
+    Fg $SolarizedBase3
+  fi
+  FlushIndicator
+else
+#######################################################################
+#                             Popup mode                              #
+#######################################################################
+  # add protection from multiple instances (test ps aux | grep ...)
+  StartPopup
+  Add "`$MCF/scripts/bin/toggl current`"
   Fg $SolarizedBase3
+  NewLine
+  FlushPopup
 fi
 
-FlushIndicator
