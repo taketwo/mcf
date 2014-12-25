31966ed14eabf49c39e07fc7dae02c98d47cc329
Sergey Alexandrov <alexandrov88@gmail.com>
2014-12-25 15:39:30 +0100

    Update powerline-shell bundle
    Remove extra space in the end of prompt (because powerline now takes care of it itself).

--- bash/090-powerline-shell.bash
+++ bash/090-powerline-shell.bash
@@ -1,6 +1,6 @@
 # Setup fancy "powerline-style" prompt
 function _update_ps1()
 {
-  export PS1="$(~/.mcf/scripts/bundle/powerline-shell/powerline-shell.py $?) "
+  export PS1="$(~/.mcf/scripts/bundle/powerline-shell/powerline-shell.py $?)"
 }
 export PROMPT_COMMAND="_update_ps1"
