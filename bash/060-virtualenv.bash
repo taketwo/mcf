40792d88a83a9d7ad603c29f67cd46e98a01c2c1
Sergey Alexandrov <alexandrov88@gmail.com>
2015-01-28 00:52:43 +0100

    Fix virtualenv setup script to handle both Ubuntu and Arch

--- bash/060-virtualenv.bash
+++ bash/060-virtualenv.bash
@@ -1,3 +1,8 @@
 export WORKON_HOME=~/.virtualenvs
 mkdir -p $WORKON_HOME
-source /etc/bash_completion.d/virtualenvwrapper
+
+if [ -f /etc/bash_completion.d/virtualenvwrapper ]; then
+  source /etc/bash_completion.d/virtualenvwrapper
+else
+  source /usr/bin/virtualenvwrapper_lazy.sh
+fi
