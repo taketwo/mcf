46309b423265e51373fc62d23fdfbb60c917f110
Sergey Alexandrov <alexandrov88@gmail.com>
2015-02-13 00:05:59 +0100

    Add identity file to msync command

--- bash/020-music-sync.bash
+++ bash/020-music-sync.bash
@@ -14,10 +14,10 @@ function msync ()
   host_ip=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
   phone_ip=`sed "s/\([0-9]*\.[0-9]*\.[0-9]*\.\)[0-9]*/\1$1/g" <<< "$host_ip"`
   if [[ $HOSTNAME == "x201" ]]; then
-    rsync -avz -e 'ssh -p 2222' --size-only --progress --delete \
+    rsync -avz -e "ssh -p 2222 -i $HOME/.ssh/Z3C" --size-only --progress --delete \
       root@$phone_ip:sdcard1/Music/ /home/sergey/Music
   else
-    rsync -avz -e 'ssh -p 2222' --size-only --progress --delete \
+    rsync -avz -e "ssh -p 2222 -i $HOME/.ssh/Z3C" --size-only --progress --delete \
       /media/Files/Music/ root@$phone_ip:sdcard1/Music
   fi
 }
