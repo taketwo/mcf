a39fddfa0461439399fc37e02c6e7546f91f5f31
Sergey Alexandrov <alexandrov88@gmail.com>
2015-05-08 10:45:02 +0200

    Add mcf.path() function

--- scripts/library/mcf.py
+++ scripts/library/mcf.py
@@ -3,3 +3,10 @@ import os
 HOME = os.path.expanduser('~')
 MCF = os.path.join(HOME, '.mcf')
 XMONAD = os.path.join(HOME, '.xmonad')
+
+
+def path(*args):
+    """
+    Generate a path relative to MCF directory.
+    """
+    return os.path.join(MCF, *args)
