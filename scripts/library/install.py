28afa4694cb71f4379ccaa8399c1a411d92fd33d
Sergey Alexandrov <alexandrov88@gmail.com>
2015-05-08 10:45:12 +0200

    Add documentation for install.py functions

--- scripts/library/install.py
+++ scripts/library/install.py
@@ -1,9 +1,22 @@
 def get_platform():
+    """
+    Get platform name in lower case (e.g. 'arch' or 'ubuntu').
+    """
     from platform import linux_distribution
     return linux_distribution()[0].lower()
 
 
 def only(platforms):
+    """
+    Decorator that enables function only on given platform(s).
+
+    Example
+    -------
+
+    @only('arch')
+    def foobar():
+        print('Doing something, but only on Arch.')
+    """
     platforms = platforms if isinstance(platforms, list) else [platforms]
     if get_platform() in platforms:
         def decorator(function):
