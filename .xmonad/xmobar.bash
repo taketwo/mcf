575807ebb43a31b20921f35ed458fddffe850c6d
Sergey Alexandrov <alexandrov88@gmail.com>
2015-05-10 23:38:47 +0200

    Add xmobar.bash with helper functions for xmobar panel

---
+++ .xmonad/xmobar.bash
@@ -0,0 +1,51 @@
+StartIndicator ()
+{
+  eval "__output=' '"
+}
+
+Color ()
+{
+  if [ "$__output" != " " ] ; then
+    eval "__output='<fc=$1,$2>$__output</fc>'"
+  fi
+}
+
+# Surround the segment with a clickable area
+# Arguments:
+#   1 Mouse button
+#   2 Command
+# Example:
+#   Action 1 "keyboard -n"
+Action ()
+{
+  if [ "$__output" != " " ] ; then
+    eval "__output='<action=\`${2}\` button=${1}>$__output</action>'"
+  fi
+}
+
+Icon ()
+{
+  eval "__output='$__output<icon=/home/sergey/.xmonad/icons/${1}.xbm/> '"
+}
+
+Symbol ()
+{
+  declare -A symbols
+  symbols["wifi"]=
+  symbols+=( ["planet"]= )
+  eval "__output='$__output${symbols[${1}]} '"
+}
+
+Add ()
+{
+  # Escape single quote with triple hat
+  eval "__output='$__output${1//\'/^^^} '"
+}
+
+FlushIndicator ()
+{
+  if [ "$__output" != " " ] ; then
+    # Undo triple hat escape
+    echo -n "${__output//^^^/\'}"
+  fi
+}
