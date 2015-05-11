e527331a03e6bd8fa0093e54e06301ab6f831ad4
Sergey Alexandrov <alexandrov88@gmail.com>
2015-05-11 09:11:41 +0200

    Add several useful functions to filesystem.py lib

--- scripts/library/filesystem.py
+++ scripts/library/filesystem.py
@@ -1,5 +1,4 @@
 import os
-import os.path
 import shutil
 
 
@@ -43,3 +42,71 @@ def link(src, dest, description=None, verbose=False):
     elif verbose:
         print('[+]', dest)
         print('   ', '->', src)
+
+
+def get_extension(filename):
+    """
+    Get the extension (with dot) of a given filename.
+
+    >>> get_extension('path/to/file.ext')
+    '.ext'
+    >>> get_extension('name.h')
+    '.h'
+    >>> get_extension('foo')
+    ''
+    """
+    fn, e = os.path.splitext(filename)
+    return e
+
+
+def get_stem(filename):
+    """
+    Get the stem of a given filename.
+
+    >>> get_stem('path/to/file.ext')
+    'file'
+    >>> get_stem('name.h')
+    'name'
+    >>> get_stem('foo')
+    'foo'
+    """
+    p, fn = os.path.split(filename)
+    fn, e = os.path.splitext(fn)
+    return fn
+
+
+def replace_extension(filename, new_ext):
+    """
+    Replace the extension in a given filename.
+
+    >>> replace_extension('path/to/file.ext', 'cpp')
+    'path/to/file.cpp'
+    >>> replace_extension('name.h', '.hpp')
+    'name.hpp'
+    >>> replace_extension('foo', 'bar')
+    'foo.bar'
+    """
+    fn, e = os.path.splitext(filename)
+    e = ('' if new_ext.startswith('.') else '.') + new_ext
+    return fn + e
+
+
+def replace_stem(filename, new_stem):
+    """
+    Replace the stem in a given filename.
+
+    >>> replace_stem('path/to/file.ext', 'replacement')
+    'path/to/replacement.ext'
+    >>> replace_stem('name.cpp', 'new')
+    'new.cpp'
+    >>> replace_stem('foo', 'bar')
+    'bar'
+    """
+    p, fn = os.path.split(filename)
+    fn, e = os.path.splitext(fn)
+    return os.path.join(p, new_stem + e)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
