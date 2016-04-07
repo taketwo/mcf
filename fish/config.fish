a55c914c6df8cb75c3525d2590245b0bdcd72f91
Sergey Alexandrov <alexandrov88@gmail.com>
2016-04-07 14:09:51 +0200

    MCF: get rid of fish

--- fish/config.fish
+++
@@ -1,5 +0,0 @@
-function fish_prompt
-  ~/.mcf/scripts/bundle/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
-  echo " "
-end
-
--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,6 +0,0 @@
-bind \e- end-of-line
-bind \e_ beginning-of-line
-bind \en forward-char
-bind \eh backward-char
-bind \ec history-search-backward
-bind \et history-search-forward
