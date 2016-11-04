5baf9f41afd5bdb33dc7f23c93cafd595ba816c0
Sergey Alexandrov <alexandrov88@gmail.com>
2016-11-04 11:07:35 +0100

    MCF: replace virtualenvwrapper with pew

--- bash/275-virtualenv.bash
+++ bash/275-virtualenv.bash
@@ -1,8 +1,2 @@
 export WORKON_HOME=~/.virtualenvs
 mkdir -p $WORKON_HOME
-
-if [ -f /etc/bash_completion.d/virtualenvwrapper ]; then
-  source /etc/bash_completion.d/virtualenvwrapper
-else
-  source /usr/bin/virtualenvwrapper_lazy.sh
-fi
