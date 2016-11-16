4ca507996deb151bacbb2162be7dd856a27d7cf3
Sergey Alexandrov <alexandrov88@gmail.com>
2016-11-16 10:03:41 +0100

    MCF: change tab binding for fzf

--- bash/975-fzf.bash
+++ bash/975-fzf.bash
@@ -2,7 +2,7 @@
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 
 # Default options
-export FZF_DEFAULT_OPTS='--ansi --select-1 --bind=alt-c:up,alt-t:down,alt-h:backward-char,alt-n:forward-char,tab:toggle-sort'
+export FZF_DEFAULT_OPTS='--ansi --select-1 --bind=alt-c:up,alt-t:down,alt-h:backward-char,alt-n:forward-char,alt-s:toggle-sort'
 
 # Setup fzf if exists
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
