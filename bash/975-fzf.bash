09ec82cb20e05a3017ad421f135d59661ae2035e
Sergey Alexandrov <alexandrov88@gmail.com>
2017-11-23 08:45:38 +0100

    MCF: update fzf+z integration

--- bash/975-fzf.bash
+++ bash/975-fzf.bash
@@ -20,11 +20,8 @@ bind '"\ec": history-search-backward'
 unalias z 2> /dev/null
 z()
 {
-  if [[ -z "$*" ]]; then
-    cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
-  else
-    _z "$@"
-  fi
+  [ $# -gt 0 ] && _z "$*" && return
+  cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
 }
 
 # fkill - kill process
