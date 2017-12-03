02766ba9293a94bac865924e2188becdfafe4113
Sergey Alexandrov <alexandrov88@gmail.com>
2017-12-03 13:06:09 +0100

    MCF: add an option to select clang version

--- bash/265-clang-as-default-compiler.bash
+++ bash/265-clang-as-default-compiler.bash
@@ -1,2 +1,8 @@
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
+if [[ -n $CLANG_VERSION ]]; then
+  suffix="-${CLANG_VERSION}"
+else
+  suffix=""
+fi
+
+export CC="/usr/bin/clang${suffix}"
+export CXX="/usr/bin/clang++${suffix}"
