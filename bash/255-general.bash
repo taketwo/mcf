5d2cd3e805f650e027ad00c6645858f5858cd880
Sergey Alexandrov <alexandrov88@gmail.com>
2018-05-07 21:11:36 -0400

    MCF: remove keyring-password and associated dependencies
    Xmobar was the only consumer of this.

--- bash/255-general.bash
+++ bash/255-general.bash
@@ -97,7 +97,7 @@ HISTFILESIZE=100000
 # backslash is removed before attempting a match. The second and subsequent
 # lines of a multi-line compound command are not tested, and are added to the
 # history regardless of the value of HISTIGNORE.
-HISTIGNORE="?:??:cd *:keyring-password *:pwd:cdd *:p"
+HISTIGNORE="?:??:cd *:pwd:cdd *:p"
 
 # }}}
 # HISTTIMEFORMAT {{{
