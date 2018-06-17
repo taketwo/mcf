d68deb8d63622c6da96d3b23e3068eaa5afc493b
Sergey Alexandrov <alexandrov88@gmail.com>
2018-06-17 19:54:10 -0400

    XMonad: add a note about how tmux is started for future reference

--- .xmonad/lib/MCF/Apps.hs
+++ .xmonad/lib/MCF/Apps.hs
@@ -8,6 +8,10 @@ module MCF.Apps
 
 import XMonad.Core
 
+-- Currently Xmonad takes care of starting tmux
+-- Consequently, if shell is spawned not through Xmonad (ssh, non-DM login), tmux is not started
+-- Resolution: exec tmux in .bashrc in *interactive* mode (see https://unix.stackexchange.com/a/113768/55482)
+
 appTerminal = "gnome-terminal -e 'tmux'"
 appBrowser  = "browser"
 appShell    = "bash"
