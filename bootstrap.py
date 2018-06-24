68435ac73420ac696bfcf7f94466d9ed1ffa46e7
Sergey Alexandrov <alexandrov88@gmail.com>
2018-06-24 17:35:27 -0400

    MCF: minor updates in bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -7,37 +7,40 @@ import subprocess
 from os.path import expanduser, join
 from shutil import which
 
-if not which('git'):
-    sys.exit('Please install git before running bootstrap script')
+if not which("git"):
+    sys.exit("Please install git before running bootstrap script")
 
-print('[*] Bootstrap MCF')
-print('')
+dest = join(expanduser("~"), ".mcf")
 
-print('[*] Obtain latest MCF sources')
+print("[*] Bootstrap MCF")
+print("")
+print("    Installation path: {}".format(dest))
+print("")
+
+print("[*] Obtain latest MCF sources")
 try:
-    if os.path.isdir('.mcf'):
-        print('    .mcf folder already exists, pulling the latest version')
-        os.chdir('.mcf')
-        cmd = 'git pull'
+    if os.path.isdir(dest):
+        print("    .mcf folder already exists, pulling the latest version")
+        os.chdir(dest)
+        cmd = "git pull"
         subprocess.check_call(cmd.split())
-        cmd = 'git submodule sync'
+        cmd = "git submodule sync"
         subprocess.check_call(cmd.split())
     else:
-        print('    cloning the repository')
-        cmd = 'git clone --recursive https://github.com/taketwo/mcf .mcf'
+        print("    cloning the repository")
+        cmd = "git clone --recursive https://github.com/taketwo/mcf {}".format(dest)
         subprocess.check_call(cmd.split())
 except subprocess.CalledProcessError:
-    os.exit('Failed to obtain MCF sources, aborting...')
-print('')
+    os.exit("Failed to obtain MCF sources, aborting...")
+print("")
 
-print('[*] Import install script')
-library = join(expanduser('~'), '.mcf/scripts/library')
-os.environ['PYTHONPATH'] = library
+print("[*] Import install script")
+library = join(dest, "scripts", "library")
+os.environ["PYTHONPATH"] = library
 sys.path.append(library)
-from package_manager import install
-print('')
+print("")
 
-print('[*] Install MCF')
-print('')
+print("[*] Install MCF")
+print("")
 
-install('mcf', verbose=True)
+__import__("package_manager").install("mcf", verbose=True)
