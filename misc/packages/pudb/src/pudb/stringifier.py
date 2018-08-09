be7e6ccd7bf19a2e7513a58eeb236c4826ca4c9b
Sergey Alexandrov <alexandrov88@gmail.com>
2018-08-09 13:53:25 -0400

    MCF: support more types in stringifier for PuDB

--- misc/packages/pudb/src/pudb/stringifier.py
+++ misc/packages/pudb/src/pudb/stringifier.py
@@ -13,15 +13,21 @@ def _dtype(dtype):
 
 
 def pudb_stringifier(obj):
+    if type(obj).__name__ == "Image" and obj.__module__ == "PIL.Image":
+        return f'PIL.Image {obj.mode} ({", ".join(map(str, obj.size))})'
     if type(obj).__name__ == "Tensor" and obj.__module__ == "torch":
         device = "⁙" if obj.device.type == "cuda" else ""
-        return f'Tensor ({", ".join(map(str, obj.shape))}) {device} {_dtype(obj.dtype)}'
+        return f'Tensor {device} {_dtype(obj.dtype)} ({", ".join(map(str, obj.shape))})'
     if type(obj).__name__ == "ndarray":
-        return f'ndarray ({", ".join(map(str, obj.shape))}) {_dtype(obj.dtype)}'
+        return f'ndarray {_dtype(obj.dtype)} ({", ".join(map(str, obj.shape))})'
     if type(obj).__name__ == "int64":
         return f'{obj}'
     if type(obj).__name__ == "function":
         return 'function'
+    if type(obj).__name__ == "module":
+        return 'module'
+    if type(obj).__name__ == "PosixPath":
+        return f'PosixPath {obj}'
     if type(obj) in [set, frozenset, list, tuple, dict]:
         return f"{type(obj).__name__} ({len(obj)})"
     return type_stringifier(obj)
