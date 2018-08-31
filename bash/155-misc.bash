e36f0a2cd3bcf949462e0a05d9dd64dc84b3f482
Sergey Alexandrov <alexandrov88@gmail.com>
2018-08-31 15:45:43 -0400

    Bash: move 'bashtimes' to a separate file in bin

--- bash/155-misc.bash
+++ bash/155-misc.bash
@@ -5,26 +5,6 @@
 
 # Stolen from "https://github.com/jdevera/dotfiles"
 
-function bashtimes()
-{
-   [[ -f /tmp/bashtimes.$$ ]] || return 1
-   awk '
-   {
-      if (NR==1) {
-         first=$2
-      }
-      last=$3
-      ms = ($3 - $2) / 1000000
-      printf( "%6.2f ms %s\n", ms, $1)
-   }
-   END {
-      printf("Total: %6.2f ms\n",  (last-first) / 1000000 )
-   }
-   ' /tmp/bashtimes.$$ |
-
-   sort -k1 -n -r
-}
-
 function highlight()
 {
    pygmentize -g -f terminal256 -P style=solarizedlight "$@"
