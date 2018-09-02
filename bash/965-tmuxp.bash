039e7d9be0368f0399abef001490e4c6764a4f06
Sergey Alexandrov <alexandrov88@gmail.com>
2018-09-01 23:06:48 -0400

    Bash: replace tmuxp/pipenv invocations with hard-coded completion setup

--- bash/965-tmuxp.bash
+++ bash/965-tmuxp.bash
@@ -1,4 +1,14 @@
 # Bash completion for tmuxp
+# The code below was produced by `_TMUXP_COMPLETE=source tmuxp`
+
+_tmuxp_completion()
+{
+  COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                 COMP_CWORD=$COMP_CWORD \
+                 _TMUXP_COMPLETE=complete $1 ) )
+  return 0
+}
+
 if hash tmuxp 2>/dev/null; then
-  eval "$(_TMUXP_COMPLETE=source tmuxp)"
+  complete -F _tmuxp_completion -o default tmuxp;
 fi
--- bash/977-pipenv.bash
+++ bash/977-pipenv.bash
@@ -1,4 +1,15 @@
-# The bash-independent part of the setup was performed in .profile
+# Bash-independent part of the setup was performed in .profile
+# The code below was produced by `pipenv --completion`
+
+_pipenv_completion()
+{
+  local IFS=$'\t'
+  COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                 COMP_CWORD=$COMP_CWORD \
+                 _PIPENV_COMPLETE=complete-bash $1 ) )
+  return 0
+}
+
 if command -v pipenv 1>/dev/null 2>&1; then
-  eval "$(pipenv --completion)"
+  complete -F _pipenv_completion -o default pipenv
 fi
