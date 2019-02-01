a7f691d3a59514dae4b823b6ac848e963763c96c
Sergey Alexandrov <alexandrov88@gmail.com>
2019-02-01 10:53:30 +0100

    MCF: implement "jump to last prompt" command in Tmux

--- bash/955-powerline-shell.bash
+++ bash/955-powerline-shell.bash
@@ -1,6 +1,10 @@
 function _update_ps1()
 {
-  export PS1="$(powerline-shell $?)"
+  # Replace the last character output by powerline-shell (which is a regular space) with a non-breaking space
+  # This is to enable the Tmux prompt jump trick: https://www.youtube.com/watch?v=uglorjY0Ntg
+  local powerline_output
+  powerline_output="$(powerline-shell $?)"
+  export PS1="${powerline_output% } "
 }
 
 if hash powerline-shell 2>/dev/null; then
