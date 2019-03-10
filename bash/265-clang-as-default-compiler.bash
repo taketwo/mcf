d47ed4f9d0b5d7dff12dc56e47b7bd6060a9fea4
Sergey Alexandrov <alexandrov88@gmail.com>
2019-03-10 18:00:43 +0100

    Bash: add new 'cxx' command to switch default compiler

--- bash/265-clang-as-default-compiler.bash
+++
@@ -1,8 +0,0 @@
-if [[ -n $CLANG_VERSION ]]; then
-  suffix="-${CLANG_VERSION}"
-else
-  suffix=""
-fi
-
-export CC="/usr/bin/clang${suffix}"
-export CXX="/usr/bin/clang++${suffix}"
--- bash/265-gcc-as-default-compiler.bash
+++
@@ -1,8 +0,0 @@
-if [[ -n $GCC_VERSION ]]; then
-  suffix="-${GCC_VERSION}"
-else
-  suffix=""
-fi
-
-export CC="/usr/bin/gcc${suffix}"
-export CXX="/usr/bin/g++${suffix}"
