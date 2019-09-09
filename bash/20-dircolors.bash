a8fced202832d1a759c737d3d4eb8592f37ad7b9
Sergey Alexandrov <alexandrov88@gmail.com>
2019-09-09 17:03:10 +0200

    Bash: use '-b' option in dircolor setup

--- bash/20-dircolors.bash
+++ bash/20-dircolors.bash
@@ -1,9 +1,9 @@
 # Enable color support of ls and grep
 if [ -x /usr/bin/dircolors ]; then
-    # Install dircolors for Solarized theme
-    test -r ~/.dircolors && eval `dircolors ~/.dircolors/dircolors.256dark`
-    alias ls='ls --color=auto'
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+  # Install dircolors for Solarized theme
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors/dircolors.256dark)"
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
