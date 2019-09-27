e3fdbde9ae5b79333171c7f2b49a4ef34465c892
Sergey Alexandrov <alexandrov88@gmail.com>
2019-09-27 21:54:06 +0200

    Polybar: add Aeolus segment

---
+++ .xmonad/aeolus_segment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# shellcheck source=polybar.bash
+source "$MCF/.xmonad/polybar.bash"
+# shellcheck source=solarized.bash
+source "$MCF/.xmonad/solarized.bash"
+
+StartSegment
+
+if aeolus vpn-is-active; then
+  Add "AEL"
+  Color "#ffffff" "$SolarizedMagenta"
+  Action 3 "aeolus vpn-stop"
+fi
+
+FlushSegment
