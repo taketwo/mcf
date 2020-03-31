93dca2a44a0023b9d0e807a704398258d691cbfe
Sergey Alexandrov <alexandrov88@gmail.com>
2020-03-31 11:09:25 +0200

    MCF: disable forgit built-in aliases

--- bash/74-forgit.bash
+++ bash/74-forgit.bash
@@ -1,4 +1,6 @@
 # Setup forgit
-# See: https://github.com/wfxr/forgit#custom-options for configuration possibilities
+# Do not use built-in aliases, we will put forgit into git subcommands
+export FORGIT_NO_ALIASES=1
+# See: https://github.com/wfxr/forgit#custom-options for other configuration possibilities
 # shellcheck source=/dev/null
 source "$MCF/external/forgit/forgit.plugin.sh"
