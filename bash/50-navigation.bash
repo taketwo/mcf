522501ba64aa88c6599d908504e43cef46eb8c1b
Sergey Alexandrov <alexandrov88@gmail.com>
2020-10-15 14:25:06 +0200

    Bash: add cdn/cdo functions to jump to newest/oldest subdirectory

--- bash/50-navigation.bash
+++ bash/50-navigation.bash
@@ -11,6 +11,18 @@ alias ......="cd ../../../../.."
 alias .......="cd ../../../../../.."
 alias ........="cd ../../../../../../.."
 
+# Change directory to the newest subdirectory
+function cdn() {
+  newest=$(find . -mindepth 1 -maxdepth 1 -type d -exec stat --printf="%Y\\t%n\\n" {} \; | sort -n -r | head -1 | cut -f2'')
+  cd "$newest" || return
+}
+
+# Change directory to the oldest subdirectory
+function cdo() {
+  oldest=$(find . -mindepth 1 -maxdepth 1 -type d -exec stat --printf="%Y\\t%n\\n" {} \; | sort -n | head -1 | cut -f2'')
+  cd "$oldest" || return
+}
+
 # Change directory to the directory of a file
 function cdf() {
   if [[ -e $1 && ! -d $1 ]]; then
