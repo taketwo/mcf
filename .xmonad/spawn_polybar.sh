2a7874b90293fd6a106c3a76ebb89a3c24ccd7b5
Sergey Alexandrov <alexandrov88@gmail.com>
2020-10-15 14:23:04 +0200

    Polybar: update pulseaudio sink for alpaca

--- .xmonad/spawn_polybar.sh
+++ .xmonad/spawn_polybar.sh
@@ -3,8 +3,8 @@
 if [[ $HOSTNAME == "ramen" ]]; then
   export ETH_INTERFACE="enp4s0"
 elif [[ $HOSTNAME == "alpaca" ]]; then
-  # Find using `pacmd list-sinks`, name field
-  export PULSEAUDIO_SINK="alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1"
+  # Find using `pacmd list-sinks | grep name`
+  export PULSEAUDIO_SINK="alsa_output.usb-C-Media_Electronics_Inc._Mpow_HC_20170817-00.iec958-stereo"
   export ETH_INTERFACE="enp4s0"
 elif [[ $HOSTNAME == "raccoon" ]]; then
   export ETH_INTERFACE="enp0s25"
