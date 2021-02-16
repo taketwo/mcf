a7372d54806a92433126c30c257174b0b8f86f2c
Sergey Alexandrov <alexandrov88@gmail.com>
2021-02-16 10:47:35 +0100

    XMonad: remove 'v4r' workspace

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -143,7 +143,6 @@ myTopics =
   , TI "mcf"      "~/.mcf"                                   (spawnShell)
   , TI "pcl"      "~/Workspace/Libraries/pcl"                (spawnShell)
   , TI "opencv"   "~/Workspace/Libraries/opencv"             (spawnShell)
-  , TI "v4r"      "~/Workspace/Projects/v4r"                 (spawnShell)
   , TI "ael"      "~/Workspace/Projects/aeolus"              (spawnShell)
   , TI "docker"   "~/Workspace/Projects/aeolus"              (spawnInShell "aeolus container-attach")
   , TI "mp3"      ""                                         (spawn "easytag" >> spawn "nautilus ~/Downloads/Torrents")
