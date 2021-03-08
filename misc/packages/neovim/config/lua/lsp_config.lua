e79cf0eeb33f1e6cbca6d15664d3681bc3bd8951
Sergey Alexandrov <alexandrov88@gmail.com>
2021-03-08 12:52:55 +0100

    Neovim: add commands to restart LSP and show log

--- misc/packages/neovim/config/lua/lsp_config.lua
+++ misc/packages/neovim/config/lua/lsp_config.lua
@@ -121,3 +121,16 @@ vim.fn.sign_define("LspDiagnosticsSignError", { text = "", texthl = "LspDiagn
 vim.fn.sign_define("LspDiagnosticsSignWarning", { text = "", texthl = "LspDiagnosticsSignWarning" })
 vim.fn.sign_define("LspDiagnosticsSignInformation", { text = "", texthl = "LspDiagnosticsSignInformation" })
 vim.fn.sign_define("LspDiagnosticsSignHint", { text = "", texthl = "LspDiagnosticsSignHint" })
+
+function _G.reload_lsp()
+  vim.lsp.stop_client(vim.lsp.get_active_clients())
+  vim.cmd [[edit]]
+end
+
+function _G.open_lsp_log()
+  local path = vim.lsp.get_log_path()
+  vim.cmd("edit " .. path)
+end
+
+vim.cmd('command! -nargs=0 LspLog call v:lua.open_lsp_log()')
+vim.cmd('command! -nargs=0 LspRestart call v:lua.reload_lsp()')
