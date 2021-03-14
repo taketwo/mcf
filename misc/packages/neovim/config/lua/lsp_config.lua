885df264e7a8e6469fcbc0f6e788325230277da8
Sergey Alexandrov <alexandrov88@gmail.com>
2021-03-14 15:07:30 +0100

    Neovim: use vim.api instead of vim.fn

--- misc/packages/neovim/config/lua/lsp_config.lua
+++ misc/packages/neovim/config/lua/lsp_config.lua
@@ -17,7 +17,7 @@ local on_attach = function(client, bufnr)
     return string.format("<cmd> lua require('telescope.builtin').%s()<CR>", picker)
   end
   local mapper = function(mode, key, result)
-    vim.fn.nvim_buf_set_keymap(0, mode, key, result, {noremap=true, silent=true})
+    vim.api.nvim_buf_set_keymap(0, mode, key, result, {noremap=true, silent=true})
   end
 
   -- Mappings.
