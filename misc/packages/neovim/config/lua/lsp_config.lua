7331e5182d0ade1b91c7549bb1d7fad1f260d46f
Sergey Alexandrov <alexandrov88@gmail.com>
2021-03-15 22:07:36 +0100

    Neovim: add 'bash-language-server' LSP support

--- misc/packages/neovim/config/lua/lsp_config.lua
+++ misc/packages/neovim/config/lua/lsp_config.lua
@@ -74,6 +74,10 @@ require'lspconfig'.clangd.setup{
     on_attach = on_attach
 }
 
+require'lspconfig'.bashls.setup{
+  on_attach = on_attach
+}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     -- Enable underline, use default values
