4692e5989d9ffa23c35c96b40793f51ba89e3fee
Sergey Alexandrov <alexandrov88@gmail.com>
2021-05-24 17:11:14 +0200

    Bash: add 'cd' and 'bd' functions for better directory navigation

--- bash/50-navigation.bash
+++ bash/50-navigation.bash
@@ -11,6 +11,30 @@ alias ......="cd ../../../../.."
 alias .......="cd ../../../../../.."
 alias ........="cd ../../../../../../.."
 
+# Change directory using pushd (pushes the current directory onto a history stack)
+# If the cd argument is a file instead of a directory it will open it for editing.
+function cd() {
+  if [ "$#" = "0" ]; then
+    pushd "$HOME" >/dev/null || return
+  elif [ -f "$1" ]; then
+    $EDITOR "$1"
+  else
+    pushd "$1" >/dev/null || return
+  fi
+}
+
+# Change back the directory (pop the top directory from a history stack)
+# Accepts a number as an argument to step back n directories at a time.
+function bd() {
+  if [ "$#" = "0" ]; then
+    popd >/dev/null || return
+  else
+    for i in $(seq "$1"); do
+      popd >/dev/null || return
+    done
+  fi
+}
+
 # Change directory to the newest subdirectory
 function cdn() {
   newest=$(find . -mindepth 1 -maxdepth 1 -type d -exec stat --printf="%Y\\t%n\\n" {} \; | sort -n -r | head -1 | cut -f2'')
