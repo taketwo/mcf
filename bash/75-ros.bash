5161e0bd386a3d0277aa404cc2908a30e304a49c
Sergey Alexandrov <alexandrov88@gmail.com>
2021-06-04 13:22:13 +0200

    Bash: remove obsolete ROS exports and aliases

--- bash/75-ros.bash
+++ bash/75-ros.bash
@@ -1,22 +1,4 @@
 : ${ROS_DISTRO:?"variable is not set!"}
 
-ROS_WORKSPACE_SETUP="$ROS_WORKSPACE_PATH/devel/setup.bash"
-
-if [ -f $ROS_WORKSPACE_SETUP ]; then
-  source $ROS_WORKSPACE_SETUP
-fi
-
-ROS_CATKIN_MAKE_REPLACEMENT="$ROS_WORKSPACE_PATH/catkin_make.sh"
-
-if [ -f $ROS_CATKIN_MAKE_REPLACEMENT ]; then
-  source $ROS_CATKIN_MAKE_REPLACEMENT
-fi
-
-export OGRE_RTT_MODE=Copy
-export OGRE_RTT_MODE=FBO
-
 function image() { rosrun image_view image_view image:="$@"; }
 function message() { rostopic echo -n 1 "$@"; }
-
-alias rosdr='rosrun rqt_reconfigure rqt_reconfigure'
-alias rviz='rosrun rviz rviz'
