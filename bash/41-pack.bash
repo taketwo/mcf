6cc07b9d8cb69890fd445529e9066ed466819c24
Sergey Alexandrov <alexandrov88@gmail.com>
2021-11-14 11:06:35 +0100

    Bash: strip trailing slash in pack()

--- bash/41-pack.bash
+++ bash/41-pack.bash
@@ -12,14 +12,15 @@
 #     7z
 function pack() {
   if [[ -f $2 || -d $2 ]]; then
+    name=${2%/}
     case $1 in
-      tbz) tar cjvf "$2.tar.bz2" "$2" ;;
-      tgz) tar czvf "$2.tar.gz" "$2" ;;
-      tar) tar cpvf "$2.tar" "$2" ;;
+      tbz) tar cjvf "$name.tar.bz2" "$2" ;;
+      tgz) tar czvf "$name.tar.gz" "$2" ;;
+      tar) tar cpvf "$name.tar" "$2" ;;
       bz2) bzip2 "$2" ;;
-      gz) gzip -c -9 -n "$2" >"$2.gz" ;;
-      zip) zip -r "$2.zip" "$2" ;;
-      7z) 7z a "$2.7z" "$2" ;;
+      gz) gzip -c -9 -n "$2" >"$name.gz" ;;
+      zip) zip -r "$name.zip" "$2" ;;
+      7z) 7z a "$name.7z" "$2" ;;
       *) echo "Do not know how to pack into '$1'..." ;;
     esac
   else
