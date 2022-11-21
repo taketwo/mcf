444638d911c92d84baca4770e36ede7b283e39ca
Sergey Alexandrov <alexandrov88@gmail.com>
2022-11-21 09:02:41 +0100

    MPM: reduce pyenv.bash startup time by caching pyenv init output

--- bash/70-pyenv.bash
+++ bash/70-pyenv.bash
@@ -1,4 +1,7 @@
 # The bash-independent part of the setup was performed in .profile
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init - --no-rehash)"
+# We don't want to pay 30 ms on every Bash shell startup, so we cache the outputs
+pyenv_init_script="/tmp/pyenv_init.bash"
+if [[ ! -f $pyenv_init_script ]]; then
+  pyenv init - --no-rehash > $pyenv_init_script
 fi
+source $pyenv_init_script
