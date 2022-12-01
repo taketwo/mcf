4245e2159e0030fb88e584c1616f8d0e76ea4f86
Sergey Alexandrov <alexandrov88@gmail.com>
2022-12-01 18:53:03 +0100

    Neovim: add keymap for showing todo comments in Telescope

--- misc/packages/neovim/config/lua/plugins/configs/todo_comments.lua
+++ misc/packages/neovim/config/lua/plugins/configs/todo_comments.lua
@@ -4,3 +4,4 @@ todo_comments.setup{}
 
 vim.keymap.set("n", "]t", todo_comments.jump_next, { desc = "Next todo comment" })
 vim.keymap.set("n", "[t", todo_comments.jump_prev, { desc = "Previous todo comment" })
+vim.api.nvim_set_keymap("n", "<Leader>.t", ":TodoTelescope<CR>", { noremap = true, silent = true, desc = "Show todo comments in Telescope" })
