849fecc98a142c3248f3b7f4289177fbad3ea001
Sergey Alexandrov <alexandrov88@gmail.com>
2022-12-07 17:03:45 +0100

    Neovim: add trigger_backlist option for which-key plugin to avoid delays on some keys

--- misc/packages/neovim/config/lua/plugins/init.lua
+++ misc/packages/neovim/config/lua/plugins/init.lua
@@ -116,6 +116,10 @@ require('packer').startup({
               -- Temporary disable operators to avoid delay on pressing "c"
               operators = false
             }
+          },
+          triggers_blacklist = {
+            i = { "c", "t", "h", "n", "." },
+            v = { "c", "t", "h", "n" },
           }
         }
       end
