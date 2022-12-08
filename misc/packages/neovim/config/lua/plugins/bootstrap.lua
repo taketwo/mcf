fb207668e55b763b4a473fd585f0c847160d9be5
Sergey Alexandrov <alexandrov88@gmail.com>
2022-12-08 21:59:10 +0100

    Neovim: perform Mason installation in the end of bootstrapping

--- misc/packages/neovim/config/lua/plugins/bootstrap.lua
+++ misc/packages/neovim/config/lua/plugins/bootstrap.lua
@@ -6,7 +6,18 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
   print( "Cloning packer...")
   fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+
+  -- Install and compile plugins
   vim.cmd [[packadd packer.nvim]]
   require('plugins')
   require('packer').sync()
+
+  -- Install language servers with Mason and Treesitter parsers
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'PackerComplete',
+    callback = function()
+      vim.cmd "bw | silent! MasonToolsInstall"
+      require('packer').loader('nvim-treesitter')
+    end,
+  })
 end
