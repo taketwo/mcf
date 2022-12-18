a07a8231ad5c6be2e6f1c36864759fafe373431e
Sergey Alexandrov <alexandrov88@gmail.com>
2022-12-18 08:50:30 +0100

    Bash: use OneDark for dircolors

--- bash/20-dircolors.bash
+++ bash/20-dircolors.bash
@@ -1,7 +1,7 @@
 # Enable color support of ls and grep
 if [ -x /usr/bin/dircolors ]; then
   # Install dircolors for Solarized theme
-  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors/dircolors.256dark)"
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors/dircolors.one-dark)"
   alias ls='ls --color=auto'
   alias grep='grep --color=auto'
   alias fgrep='fgrep --color=auto'
