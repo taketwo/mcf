a3324491f43e22ca2e7d84d3aa7c723e3b9c5572
Sergey Alexandrov <alexandrov88@gmail.com>
2022-12-19 21:58:29 +0100

    Neovim: partial cleanup of mappings for gitsigns

--- misc/packages/neovim/config/lua/plugins/config/gitsigns.lua
+++ misc/packages/neovim/config/lua/plugins/config/gitsigns.lua
@@ -2,18 +2,25 @@ require('gitsigns').setup{
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
-    -- TODO: Move some of the keymaps out of 'h' subsection
+    -- TODO: Make the mappings local to the buffer
     require('which-key').register{
-      ['<Leader>gh'] = {
-        name = '+hunks',
-        b = { function() gs.blame_line{full=true} end, 'Blame' },
-        d = { gs.diffthis, 'Diff' },
-        p = { gs.preview_hunk, 'Preview' },
-        r = { '<cmd>Gitsigns reset_hunk<cr>', 'Reset' },
-        s = { '<cmd>Gitsigns stage_hunk<cr>', 'Stage' },
-        tb = { gs.toggle_current_line_blame, 'Toggle current line blame' },
-        td = { gs.toggle_deleted, 'Toggle deleted lines' },
-        u = { gs.undo_stage_hunk, 'Unstage' },
+      ['<Leader>'] = {
+        g = {
+          name = 'Git',
+          b = { '<cmd>Git blame<cr>', 'Blame' },
+          h = {
+            name = '+hunks',
+            d = { gs.diffthis, 'Diff' },
+            p = { gs.preview_hunk, 'Preview' },
+            r = { '<cmd>Gitsigns reset_hunk<cr>', 'Reset' },
+            s = { '<cmd>Gitsigns stage_hunk<cr>', 'Stage' },
+            u = { gs.undo_stage_hunk, 'Unstage' },
+          },
+          -- TODO: The one below is an alternative to git-messenger, consider removing one of them
+          B = { function() gs.blame_line{full=true} end, 'Blame' },
+          tb = { gs.toggle_current_line_blame, 'Toggle current line blame' },
+          td = { gs.toggle_deleted, 'Toggle deleted lines' },
+        },
       },
       [']h'] = { function()
           if vim.wo.diff then return ']h' end
