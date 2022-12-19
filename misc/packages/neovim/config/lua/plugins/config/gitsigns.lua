a42b18a3fe93eff0a959e8b91772789fd7061bee
Sergey Alexandrov <alexandrov88@gmail.com>
2022-12-19 13:35:59 +0100

    Neovim: replace 'gitgutter' with 'gitsigns'

---
+++ misc/packages/neovim/config/lua/plugins/config/gitsigns.lua
@@ -0,0 +1,39 @@
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    -- TODO: Move some of the keymaps out of 'h' subsection
+    require('which-key').register{
+      ['<Leader>gh'] = {
+        name = '+hunks',
+        b = { function() gs.blame_line{full=true} end, 'Blame' },
+        d = { gs.diffthis, 'Diff' },
+        p = { gs.preview_hunk, 'Preview' },
+        r = { '<cmd>Gitsigns reset_hunk<cr>', 'Reset' },
+        s = { '<cmd>Gitsigns stage_hunk<cr>', 'Stage' },
+        tb = { gs.toggle_current_line_blame, 'Toggle current line blame' },
+        td = { gs.toggle_deleted, 'Toggle deleted lines' },
+        u = { gs.undo_stage_hunk, 'Unstage' },
+      },
+      [']h'] = { function()
+          if vim.wo.diff then return ']h' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, 'Next git hunk', expr=true },
+      ['[h'] = { function()
+          if vim.wo.diff then return '[h' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, 'Previous git hunk', expr=true },
+    }
+
+    -- Text object
+    -- TODO: Switch to using 'which-key'
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
