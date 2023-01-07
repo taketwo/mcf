7c2b3fbd51a1932f0b46713b03dc22ed01a41253
Sergey Alexandrov <alexandrov88@gmail.com>
2023-01-07 21:14:11 +0100

    Neovim: open Telescope results in Trouble with <c-t>

--- misc/packages/neovim/config/lua/plugins/config/telescope.lua
+++ misc/packages/neovim/config/lua/plugins/config/telescope.lua
@@ -1,4 +1,5 @@
 local actions = require('telescope.actions')
+local trouble = require('trouble.providers.telescope')
 local telescope = require('telescope')
 
 telescope.setup({
@@ -10,6 +11,10 @@ telescope.setup({
         ['<A-n>'] = actions.preview_scrolling_down,
         ['<A-h>'] = actions.preview_scrolling_up,
         ['<Esc>'] = actions.close,
+        ['<c-t>'] = trouble.open_with_trouble,
+      },
+      n = {
+        ['<c-t>'] = trouble.open_with_trouble,
       },
     },
   },
