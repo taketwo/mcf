aa785fe8a73c1f48314b908e3e5c1de1abf509c4
Sergey Alexandrov <alexandrov88@gmail.com>
2023-02-08 17:00:06 +0100

    Neovim: add SortMotion plugin

---
+++ misc/packages/neovim/config/lua/plugins/vim-sort-motion.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'christoomey/vim-sort-motion',
+    keys = {
+      { 'gs', desc = 'Sort lines with motion' },
+      { 'gs', mode = 'x', desc = 'Sort lines in visual selection' },
+    },
+  },
+}
