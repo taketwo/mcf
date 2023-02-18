90352d17a9d70928eb5008b0cf1c6f840d05b34f
Sergey Alexandrov <alexandrov88@gmail.com>
2023-02-18 10:48:06 +0100

    Neovim: add utility for creating keymaps that toggle options

--- misc/packages/neovim/config/lua/utils.lua
+++ misc/packages/neovim/config/lua/utils.lua
@@ -1,3 +1,8 @@
+-- Some of the functions in this file were adapted from LazyVim
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/util/init.lua
+
+local Util = require('lazy.core.util')
+
 local M = {}
 
 --- Extract short plugin name from the plugin location string (e.g. 'nvim-lua/plenary.nvim' -> 'plenary')
@@ -10,6 +15,27 @@ M.extract_plugin_name = function(plugin_location)
     :gsub('%.vim$', '')
     :gsub('^vim%-', '')
     :gsub('^nvim%-', '')
+end
+
+---@param silent boolean?
+---@param values? {[1]:any, [2]:any}
+function M.toggle(option, silent, values)
+  if values then
+    if vim.opt_local[option]:get() == values[1] then
+      vim.opt_local[option] = values[2]
+    else
+      vim.opt_local[option] = values[1]
+    end
+    return Util.info('Set ' .. option .. ' to ' .. vim.opt_local[option]:get(), { title = 'Option' })
+  end
+  vim.opt_local[option] = not vim.opt_local[option]:get()
+  if not silent then
+    if vim.opt_local[option]:get() then
+      Util.info('Enabled ' .. option, { title = 'Option' })
+    else
+      Util.warn('Disabled ' .. option, { title = 'Option' })
+    end
+  end
 end
 
 return M
