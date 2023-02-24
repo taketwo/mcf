acab14f55709ebe1460dec7da19b98a080daa439
Sergey Alexandrov <alexandrov88@gmail.com>
2023-02-24 21:40:37 +0100

    Neovim: remove 'event' and 'keys' in lspsaga
    Lazy-loading will happen on :Lspsaga command.

--- misc/packages/neovim/config/lua/plugins/lspsaga-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/lspsaga-nvim.lua
@@ -1,13 +1,7 @@
 return {
   {
     'glepnir/lspsaga.nvim',
-    event = "BufRead", -- TODO: Load only when LSP is attached
     cmd = { 'Lspsaga' },
-    keys = {
-      { '<Leader>lD', '<cmd>Lspsaga show_line_diagnostics<cr>', 'Line diagnostics' },
-      { '<Leader>ld', '<cmd>Lspsaga peek_definition<cr>', 'Definition preview' },
-      { '<Leader>lf', '<cmd>Lspsaga lsp_finder<cr>', 'Finder' },
-    },
     opts = {
       symbol_in_winbar = {
         enable = false,
