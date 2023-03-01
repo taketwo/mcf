629b854ede479ba8fc4e6ec2b0768ec02be0b6a1
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-01 16:53:19 +0100

    Neovim: fix Git blame keymap description

--- misc/packages/neovim/config/lua/plugins/vim-fugitive.lua
+++ misc/packages/neovim/config/lua/plugins/vim-fugitive.lua
@@ -3,7 +3,7 @@ return {
     'tpope/vim-fugitive',
     cmd = { 'Git', 'Gedit' },
     keys = {
-      { '<Leader>gb', '<cmd>Git blame<cr>', 'Blame' },
+      { '<Leader>gb', '<cmd>Git blame<cr>', desc = 'Blame' },
     },
   },
 }
