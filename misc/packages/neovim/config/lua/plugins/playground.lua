2e254cc3136f8d4f60bb04b41a7838720ea3460a
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-03 19:13:32 +0100

    Neovim: lazy-load Treesitter Playground on command

--- misc/packages/neovim/config/lua/plugins/playground.lua
+++ misc/packages/neovim/config/lua/plugins/playground.lua
@@ -1,8 +1,7 @@
 return {
   {
     'nvim-treesitter/playground',
-    event = { 'BufReadPost', 'BufNewFile' },
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    -- NOTE: Config is in nvim-treesitter.lua
+    cmd = 'TSPlaygroundToggle',
   },
 }
