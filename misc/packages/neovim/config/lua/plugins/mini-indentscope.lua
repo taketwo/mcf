9b8e0bc84517fefbb53880decf95a638a638d99d
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-06 21:44:30 +0100

    Neovim: disable animation in mini.indentscope

--- misc/packages/neovim/config/lua/plugins/mini-indentscope.lua
+++ misc/packages/neovim/config/lua/plugins/mini-indentscope.lua
@@ -7,6 +7,9 @@ return {
       options = {
         try_as_border = true, -- Get scope of function body by placing cursor on its header
       },
+      draw = {
+        animation = function() return 0 end,
+      },
     },
     init = function()
       vim.api.nvim_create_autocmd('FileType', {
