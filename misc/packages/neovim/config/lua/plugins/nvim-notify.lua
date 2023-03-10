c10cfd3601add18e2e26eda6953487b2379e3085
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-10 10:48:15 +0100

    Neovim: add nvim-notify plugin

---
+++ misc/packages/neovim/config/lua/plugins/nvim-notify.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'rcarriga/nvim-notify',
+    event = 'VeryLazy',
+    keys = {
+      {
+        '<Leader>un',
+        function() require('notify').dismiss({ silent = true, pending = true }) end,
+        desc = 'Dismiss notifications',
+      },
+    },
+    opts = {
+      timeout = 3000,
+      max_height = function() return math.floor(vim.o.lines * 0.75) end,
+      max_width = function() return math.floor(vim.o.columns * 0.75) end,
+      background_colour = '#000000',
+    },
+    config = function(_, opts)
+      -- NOTE: May need to remove this if adopting Noice
+      vim.notify = require('notify')
+      vim.notify.setup(opts)
+    end,
+  },
+}
