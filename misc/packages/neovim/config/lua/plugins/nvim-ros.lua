18c8fb10fad160d17447253e5df17b8d5a004d09
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-10 22:34:12 +0100

    Neovim: set keys for nvim-ros

--- misc/packages/neovim/config/lua/plugins/nvim-ros.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-ros.lua
@@ -1,7 +1,10 @@
 return {
   {
     'taketwo/nvim-ros',
-    event = { 'BufReadPre', 'BufNewFile' },
+    keys = {
+      { '<Leader>re', '<cmd>Telescope ros ed<cr>', desc = 'Open file in package' },
+      { '<Leader>rm', '<cmd>Telescope ros msg<cr>', desc = 'Open message' },
+    },
     opts = {
       log_level = 'trace',
     },
