32ffe1ec795189b4edf3cdb0235dd8615a16d457
Sergey Alexandrov <alexandrov88@gmail.com>
2023-03-22 20:49:56 +0100

    Neovim: add DAP configu to launch ROS executables

--- misc/packages/neovim/config/lua/config/dap/cpp.lua
+++ misc/packages/neovim/config/lua/config/dap/cpp.lua
@@ -3,6 +3,44 @@ local M = {}
 function M.setup()
   local dap = require('dap')
   dap.configurations.cpp = {
+    setmetatable({
+      name = 'Launch ROS executable',
+      type = 'cppdbg',
+      request = 'launch',
+      cwd = '${workspaceFolder}',
+      stopAtEntry = true,
+      setupCommands = {
+        {
+          description = 'Enable pretty-printing for gdb',
+          text = '-enable-pretty-printing',
+          ignoreFailures = true,
+        },
+      },
+    }, {
+      __call = function(config)
+        local ros_package_name = vim.b.ros_package_name
+        local ros_package_workspace = vim.b.ros_package_workspace
+        if ros_package_name and ros_package_workspace then
+          local executable_path = ros_package_workspace
+            .. '/devel/lib/'
+            .. ros_package_name
+            .. '/'
+            .. vim.fn.fnamemodify(vim.fn.expand('%'), ':t:r')
+          config.program = executable_path
+        end
+        config.args = vim.split(
+          vim.fn.input({
+            prompt = 'Arguments: ',
+          }),
+          ' '
+        )
+        local config_copy = vim.deepcopy(config)
+        setmetatable(config_copy, nil)
+        config_copy.name = config_copy.name .. ' (again)'
+        table.insert(dap.configurations.cpp, 1, config_copy)
+        return config
+      end,
+    }),
     {
       name = 'Launch file',
       type = 'cppdbg',
