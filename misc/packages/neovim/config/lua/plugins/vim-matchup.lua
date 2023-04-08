b935d7b512edbfffa90f743e3e9e038e57e1ea20
Sergey Alexandrov <alexandrov88@gmail.com>
2023-04-08 13:20:02 +0200

    Neovim: completely disable display of matching paren in vim-matchup

--- misc/packages/neovim/config/lua/plugins/vim-matchup.lua
+++ misc/packages/neovim/config/lua/plugins/vim-matchup.lua
@@ -1,6 +1,6 @@
 return {
   {
     'andymass/vim-matchup',
-    init = function() vim.g.matchup_matchparen_offscreen = { method = 'popup' } end,
+    init = function() vim.g.matchup_matchparen_offscreen = {} end,
   },
 }
