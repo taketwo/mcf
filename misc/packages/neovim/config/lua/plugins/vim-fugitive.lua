6a0915eb361cfdc569d60d3d12fb388a51c242a7
Sergey Alexandrov <alexandrov88@gmail.com>
2023-04-19 17:37:55 +0200

    Neovim: add keymap to reset buffer state

--- misc/packages/neovim/config/lua/plugins/vim-fugitive.lua
+++ misc/packages/neovim/config/lua/plugins/vim-fugitive.lua
@@ -4,6 +4,7 @@ return {
     cmd = { 'Git', 'Gedit' },
     keys = {
       { '<Leader>gb', '<cmd>Git blame<cr>', desc = 'Blame' },
+      { '<Leader>gr', '<cmd>Git restore %<cr>', desc = 'Reset buffer' },
     },
   },
 }
