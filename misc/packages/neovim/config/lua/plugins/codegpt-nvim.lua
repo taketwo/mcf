a567caa9b0d815b382174bc4e4e035955baf791a
Sergey Alexandrov <alexandrov88@gmail.com>
2023-04-21 21:11:33 +0200

    Neovim: send codegpt.nvim output to vertical split

--- misc/packages/neovim/config/lua/plugins/codegpt-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/codegpt-nvim.lua
@@ -6,5 +6,10 @@ return {
       { 'MunifTanjim/nui.nvim' },
     },
     cmd = 'Chat',
+    config = function()
+      require('codegpt.config')
+      vim.g.codegpt_popup_type = 'vertical'
+      vim.g.codegpt_vertical_popup_size = '30%'
+    end,
   },
 }
