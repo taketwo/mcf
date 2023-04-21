ae68bac789e8e769051f355ce12b3a73a93cebbd
Sergey Alexandrov <alexandrov88@gmail.com>
2023-04-21 21:11:00 +0200

    Neovim: add chatgpt.nvim plugin

---
+++ misc/packages/neovim/config/lua/plugins/chatgpt-nvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'jackMort/ChatGPT.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    cmd = { 'ChatGPT', 'ChatGPTActAs', 'ChatGPTEditWithInstructions', 'ChatGPTRun' },
+  },
+}
