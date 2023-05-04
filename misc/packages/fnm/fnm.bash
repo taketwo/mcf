2a686c1df694b652eca389e063f08cd377e525e9
Sergey Alexandrov <alexandrov88@gmail.com>
2023-05-04 07:33:58 +0200

    MCF: ensure that fnm initialization works if executed from several bash instances

--- misc/packages/fnm/fnm.bash
+++ misc/packages/fnm/fnm.bash
@@ -2,12 +2,16 @@
 
 fnm_complete_script="/tmp/fnm-complete.bash"
 if [[ ! -f $fnm_complete_script ]]; then
-  fnm completions --shell bash > $fnm_complete_script
+  tmp=$(mktemp "/tmp/fnm-complete.XXX.bash")
+  fnm completions --shell bash >"$tmp"
+  mv "$tmp" $fnm_complete_script
 fi
 source $fnm_complete_script
 
 fnm_env_script="/tmp/fnm-env.bash"
 if [[ ! -f $fnm_env_script ]]; then
-  fnm env > $fnm_env_script
+  tmp=$(mktemp "/tmp/fnm-env.XXX.bash")
+  fnm env >"$tmp"
+  mv "$tmp" $fnm_env_script
 fi
 source $fnm_env_script
