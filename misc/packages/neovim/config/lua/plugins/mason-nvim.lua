f8956fb96cc7bf2ef47a02dffa5f3e29c935e96b
Sergey Alexandrov <alexandrov88@gmail.com>
2023-05-05 21:02:07 +0200

    Neovim: add taplo LSP for TOML files

--- misc/packages/neovim/config/lua/plugins/mason-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/mason-nvim.lua
@@ -17,6 +17,7 @@ return {
         'shellcheck',
         'shfmt',
         'stylua',
+        'taplo',
         'usort',
       },
     },
--- misc/packages/neovim/config/lua/plugins/nvim-lspconfig.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-lspconfig.lua
@@ -50,6 +50,7 @@ return {
             documentFormattingProvider = false,
           },
         },
+        taplo = {},
       }
 
       local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
