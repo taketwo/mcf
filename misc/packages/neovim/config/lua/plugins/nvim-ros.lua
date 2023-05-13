4fe003610b8a444b058b5cb05c933e96f8cec8d7
Sergey Alexandrov <alexandrov88@gmail.com>
2023-05-13 21:51:37 +0200

    Neovim: specify vim-ros as dependency for nvim-ros

--- misc/packages/neovim/config/lua/plugins/nvim-ros.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-ros.lua
@@ -5,6 +5,10 @@ return {
       { '<Leader>re', '<cmd>Telescope ros ed<cr>', desc = 'Open file in package' },
       { '<Leader>rm', '<cmd>Telescope ros msg<cr>', desc = 'Open message' },
     },
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'taketwo/vim-ros',
+    },
     opts = {
       log_level = 'trace',
     },
