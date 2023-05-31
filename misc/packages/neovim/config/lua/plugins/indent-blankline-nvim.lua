fe71a41ec31bdf83b6dfd68717781bb8841c37fb
Sergey Alexandrov <alexandrov88@gmail.com>
2023-05-31 10:57:39 +0200

    Neovim: add 'notify' filetype to the list of exclusions for indentation lines

--- misc/packages/neovim/config/lua/plugins/indent-blankline-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/indent-blankline-nvim.lua
@@ -4,7 +4,7 @@ return {
     event = { 'BufReadPost', 'BufNewFile' },
     opts = {
       char_highlight_list = {},
-      filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' },
+      filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason', 'notify' },
       show_current_context = false,
       show_trailing_blankline_indent = false,
       space_char_blankline = ' ',
