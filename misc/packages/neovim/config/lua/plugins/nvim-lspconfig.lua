6dd210c5b73997f517342286de34348f162a0c3c
Sergey Alexandrov <alexandrov88@gmail.com>
2023-07-04 22:07:41 +0200

    Neovim: add new clangd options
    Based on LazyVim config, untested.

--- misc/packages/neovim/config/lua/plugins/nvim-lspconfig.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-lspconfig.lua
@@ -28,10 +28,21 @@ return {
       local servers = {
         bashls = {},
         clangd = {
-          cmd = { 'clangd', '--background-index', '--completion-style=bundled', '--clang-tidy' },
+          cmd = {
+            'clangd',
+            '--background-index',
+            '--clang-tidy',
+            '--completion-style=bundled', -- TODO: Check if detailed style is better
+            '--function-arg-placeholders', -- TODO: Understand whether this is useful
+          },
           filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'gtest.cpp' },
           capabilities = {
             offsetEncoding = 'utf-16',
+          },
+          -- Some of the supported options are listed at: https://clangd.llvm.org/extensions
+          init_options = {
+            usePlaceholders = true, -- TODO: Understand whether this is useful
+            completeUnimported = true, -- TODO: Understand whether this is useful
           },
         },
         jedi_language_server = {},
