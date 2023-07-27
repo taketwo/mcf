e7322d7dc6a8ed6221c01fe0f119ac0101907794
Sergey Alexandrov <alexandrov88@gmail.com>
2023-07-27 07:54:30 +0200

    Neovim: disable quickfix window in vimtex

--- misc/packages/neovim/config/lua/plugins/vimtex.lua
+++ misc/packages/neovim/config/lua/plugins/vimtex.lua
@@ -8,6 +8,7 @@ return {
       vim.g.vimtex_compiler_enabled = 1
       vim.g.vimtex_compiler_method = 'tectonic'
       vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_quickfix_enabled = 0
     end,
     -- TODO: Plugin has a bunch of useful commands that can be mapped
   },
