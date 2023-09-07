a51874dad4305a01395777f0a23a403d20fa5cb5
Sergey Alexandrov <alexandrov88@gmail.com>
2023-09-07 17:20:31 +0200

    Neovim: do not go to last edit location in gitrebase buffers

--- misc/packages/neovim/config/lua/config/autocmds.lua
+++ misc/packages/neovim/config/lua/config/autocmds.lua
@@ -26,7 +26,7 @@ vim.api.nvim_create_autocmd({ 'VimResized' }, {
 vim.api.nvim_create_autocmd('BufReadPost', {
   group = augroup('last_loc'),
   callback = function()
-    local exclude = { 'gitcommit' }
+    local exclude = { 'gitcommit', 'gitrebase' }
     local buf = vim.api.nvim_get_current_buf()
     if vim.tbl_contains(exclude, vim.bo[buf].filetype) then return end
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
