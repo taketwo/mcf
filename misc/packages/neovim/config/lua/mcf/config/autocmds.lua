b5557da19a7bc579224f4fafac46024c02e6586d
Sergey Alexandrov <alexandrov88@gmail.com>
2023-10-11 13:10:39 +0200

    Neovim: prevent executing last_loc autocommand more than once

--- misc/packages/neovim/config/lua/mcf/config/autocmds.lua
+++ misc/packages/neovim/config/lua/mcf/config/autocmds.lua
@@ -28,7 +28,8 @@ vim.api.nvim_create_autocmd('BufReadPost', {
   callback = function(event)
     local exclude = { 'gitcommit', 'gitrebase' }
     local buf = event.buf
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then return end
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc_done then return end
+    vim.b[buf].last_loc_done = true
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then pcall(vim.api.nvim_win_set_cursor, 0, mark) end
