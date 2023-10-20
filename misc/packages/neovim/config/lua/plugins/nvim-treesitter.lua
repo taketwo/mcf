a1c54b803c7b420c965a15cf5524163de2bba3e1
Sergey Alexandrov <alexandrov88@gmail.com>
2023-10-20 11:41:15 +0200

    Neovim: limit number of lines of TS context

--- misc/packages/neovim/config/lua/plugins/nvim-treesitter.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       { 'nvim-treesitter/nvim-treesitter-textobjects' },
-      { 'nvim-treesitter/nvim-treesitter-context' },
+      { 'nvim-treesitter/nvim-treesitter-context', opts = { max_lines = 3 } },
     },
     build = ':TSUpdate',
     event = { 'BufReadPost', 'BufNewFile' },
