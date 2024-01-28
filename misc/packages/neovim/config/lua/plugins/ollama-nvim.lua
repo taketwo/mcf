b2a328864d4464e9ad3e754bee65ffc09334f508
Sergey Alexandrov <alexandrov88@gmail.com>
2024-01-28 16:47:32 +0100

    Neovim: add 'ollama.nvim' plugin

---
+++ misc/packages/neovim/config/lua/plugins/ollama-nvim.lua
@@ -0,0 +1,26 @@
+return {
+  'nomnivore/ollama.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  cmd = { 'Ollama', 'OllamaModel' },
+  keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+    {
+      '<leader>oo',
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = 'ollama prompt',
+      mode = { 'n', 'v' },
+    },
+    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    {
+      '<leader>oG',
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = 'ollama Generate Code',
+      mode = { 'n', 'v' },
+    },
+  },
+  opts = {
+    model = 'stable-code',
+  },
+}
