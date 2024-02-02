50a84b774b2be215a904addcd10a5f6ee844e133
Sergey Alexandrov <alexandrov88@gmail.com>
2024-02-02 11:05:38 +0100

    Neovim: add more PearTree mappings

--- misc/packages/neovim/config/lua/plugins/pear-tree.lua
+++ misc/packages/neovim/config/lua/plugins/pear-tree.lua
@@ -3,8 +3,10 @@ return {
     'tmsvg/pear-tree',
     event = 'InsertEnter',
     keys = {
-      { '<BS>', '<Plug>(PearTreeBackspace)', mode = 'i' },
-      { '<C-s>', '<Plug>(PearTreeJump)', mode = 'i' },
+      { '<BS>', '<Plug>(PearTreeBackspace)', mode = 'i', desc = 'Delete character before cursor' },
+      { '<CR>', '<Plug>(PearTreeExpand)', mode = 'i', desc = 'Begin new line' },
+      { '<Space>', '<Plug>(PearTreeSpace)', mode = 'i', desc = 'Insert space' },
+      { '<C-s>', '<Plug>(PearTreeJump)', mode = 'i', desc = 'Jump past closing pair' },
     },
     init = function()
       vim.g.pear_tree_repeatable_expand = 0
