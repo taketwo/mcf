e2203712be5ed3d9fdc8b9b71a6375ee1772b54b
Sergey Alexandrov <alexandrov88@gmail.com>
2024-02-02 11:32:56 +0100

    Neovim: disable PearTree in Telescope prompt

--- misc/packages/neovim/config/lua/plugins/pear-tree.lua
+++ misc/packages/neovim/config/lua/plugins/pear-tree.lua
@@ -4,13 +4,21 @@ return {
     -- NOTE: This plugin ships with a number of ftplugins that customize auto-pairing behavior for
     -- specific filetypes. We need to disable lazy loading to ensure that the ftplugins are loaded.
     lazy = false,
-    keys = {
-      { '<BS>', '<Plug>(PearTreeBackspace)', mode = 'i', desc = 'Delete character before cursor' },
-      { '<CR>', '<Plug>(PearTreeExpand)', mode = 'i', desc = 'Begin new line' },
-      { '<Space>', '<Plug>(PearTreeSpace)', mode = 'i', desc = 'Insert space' },
-      { '<C-s>', '<Plug>(PearTreeJump)', mode = 'i', desc = 'Jump past closing pair' },
-    },
+    keys = function()
+      local keys = {
+        { '<BS>', '<Plug>(PearTreeBackspace)', desc = 'Delete character before cursor' },
+        { '<CR>', '<Plug>(PearTreeExpand)', desc = 'Begin new line' },
+        { '<Space>', '<Plug>(PearTreeSpace)', desc = 'Insert space' },
+        { '<C-s>', '<Plug>(PearTreeJump)', desc = 'Jump past closing pair' },
+      }
+      for _, key in ipairs(keys) do
+        key.mode = 'i'
+        key.ft = '!TelescopePrompt'
+      end
+      return keys
+    end,
     init = function()
+      vim.g.pear_tree_ft_disabled = { 'TelescopePrompt' }
       vim.g.pear_tree_repeatable_expand = 0
       vim.g.pear_tree_map_special_keys = 0
     end,
