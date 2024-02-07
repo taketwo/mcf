3e552de74492055cadd0d562a0eaa7937d49e276
Sergey Alexandrov <alexandrov88@gmail.com>
2024-02-07 12:34:57 +0100

    Starship: cache generated init script to accelerate startup

--- misc/packages/starship/starship.bash
+++ misc/packages/starship/starship.bash
@@ -1,1 +1,6 @@
-eval "$(starship init bash)"
+# We don't want to pay 20 ms on every Bash shell startup, so we cache the outputs
+starship_init_script="/tmp/starship_init.bash"
+if [[ ! -f $starship_init_script ]]; then
+  starship init bash --print-full-init >$starship_init_script
+fi
+source $starship_init_script
