b6dab27033f4446d2827411c1e66c1966c6bacd1
Sergey Alexandrov <alexandrov88@gmail.com>
2024-02-09 15:13:49 +0100

    Neovim: add commenting support for hjson files

--- misc/packages/neovim/config/lua/plugins/comment-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/comment-nvim.lua
@@ -2,6 +2,9 @@ return {
   {
     'numToStr/Comment.nvim',
     event = 'BufWinEnter',
-    config = function() require('Comment').setup() end,
+    config = function()
+      require('Comment').setup()
+      require('Comment.ft').hjson = '#%s'
+    end,
   },
 }
