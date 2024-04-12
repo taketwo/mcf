caa3ed83952999db3121dc09bb81e36f0f408240
Sergey Alexandrov <alexandrov88@gmail.com>
2024-04-12 13:01:21 +0200

    MCF: enable copying commit hashes to clipboard frmo forgit

--- bash/74-forgit.bash
+++ bash/74-forgit.bash
@@ -1,6 +1,8 @@
 # Setup forgit
 # Do not use built-in aliases, we will put forgit into git subcommands
 export FORGIT_NO_ALIASES=1
+# Enable copying commit hashes to clipboard
+export FORGIT_COPY_CMD='xsel --clipboard --input'
 # See: https://github.com/wfxr/forgit#custom-options for other configuration possibilities
 # shellcheck source=/dev/null
 source "$MCF/external/forgit/forgit.plugin.sh"
