29609141942f66e9912de41753fc0ec532586de8
Sergey Alexandrov <alexandrov88@gmail.com>
2024-05-21 09:46:11 +0200

    Neovim: use utility function from LazyVim to confirm cmp completion

--- misc/packages/neovim/config/lua/plugins/nvim-cmp.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-cmp.lua
@@ -13,6 +13,7 @@ return {
     },
     opts = function()
       local cmp = require('cmp')
+      local Util = require('mcf.util')
       return {
         snippet = {
           expand = function(args) vim.fn['UltiSnips#Anon'](args.body) end,
@@ -23,13 +24,7 @@ return {
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-h>'] = cmp.mapping.close(),
-          ['<C-n>'] = function(fallback)
-            if cmp.visible() then
-              require('mcf.util').create_undo()
-              if cmp.confirm({ select = true }) then return end
-            end
-            return fallback()
-          end,
+          ['<C-n>'] = Util.cmp.confirm(),
           ['<C-t>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
           ['<C-CR>'] = function(fallback)
             cmp.abort()
