7b2d3380056a552d33d028c8c418573e9c7c1669
Sergey Alexandrov <alexandrov88@gmail.com>
2024-06-14 15:11:42 +0200

    Neovim: switch to new version of venv-selector.nvim

--- misc/packages/neovim/config/lua/plugins/venv-selector-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/venv-selector-nvim.lua
@@ -1,13 +1,14 @@
 return {
   {
     'linux-cultist/venv-selector.nvim',
+    branch = 'regexp',
+    ft = 'python',
     cmd = { 'VenvSelect', 'VenvSelectCached', 'VenvSelectCurrent' },
     opts = {
-      -- Abuse venvwrapper to get the venvs from pipx
-      venvwrapper_path = vim.fn.expand('$HOME') .. '/.local/pipx/venvs',
-      name = {
-        'venv',
-        '.venv',
+      settings = {
+        options = {
+          notify_user_on_venv_activation = true,
+        },
       },
     },
   },
