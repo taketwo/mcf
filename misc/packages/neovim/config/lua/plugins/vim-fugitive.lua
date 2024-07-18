3442a5cabbd8375f1d6e273e802c414279ce4a1c
Sergey Alexandrov <alexandrov88@gmail.com>
2024-07-18 17:49:39 +0200

    Neovim: add more trigger commands for vim-fugitive plugin

--- misc/packages/neovim/config/lua/plugins/vim-fugitive.lua
+++ misc/packages/neovim/config/lua/plugins/vim-fugitive.lua
@@ -1,7 +1,7 @@
 return {
   {
     'tpope/vim-fugitive',
-    cmd = { 'Git', 'Gedit' },
+    cmd = { 'Git', 'Gedit', 'Gdiffsplit', 'Gvdiffsplit', 'Ghdiffsplit' },
     keys = {
       { '<Leader>g', '', desc = 'Git' },
       { '<Leader>gr', '<cmd>Git restore %<cr>', desc = 'Reset buffer' },
