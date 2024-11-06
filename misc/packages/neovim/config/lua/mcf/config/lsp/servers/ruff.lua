c7412fcad021a91a352534c19361af2eb5780089
Sergey Alexandrov <alexandrov88@gmail.com>
2024-11-06 11:48:14 +0100

    Neovim: switch from deprecated ruff_lsp to ruff

--- misc/packages/neovim/config/lua/mcf/config/lsp/servers/ruff.lua
+++ misc/packages/neovim/config/lua/mcf/config/lsp/servers/ruff.lua
@@ -1,7 +1,8 @@
 return {
   init_options = {
+    -- List of supported settings: https://docs.astral.sh/ruff/editors/settings/
     settings = {
-      args = { '--config', vim.fn.stdpath('config') .. '/extras/ruff.toml' },
+      configuration = vim.fn.stdpath('config') .. '/extras/ruff.toml',
       fixAll = false,
       organizeImports = false,
     },
