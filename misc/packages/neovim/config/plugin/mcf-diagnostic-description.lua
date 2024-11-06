6b9848775c906c735472ef4372ca5227f310c53a
Sergey Alexandrov <alexandrov88@gmail.com>
2024-11-06 22:12:46 +0100

    Neovim: update mcf-diagnostic-description to not use deprecated vim.lsp.diagnostic.get_line_diagnostics()

--- misc/packages/neovim/config/plugin/mcf-diagnostic-description.lua
+++ misc/packages/neovim/config/plugin/mcf-diagnostic-description.lua
@@ -1,8 +1,8 @@
 function _G.show_diagnostic_description()
-  local diagnostics = vim.lsp.diagnostic.get_line_diagnostics()
+  local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
   if #diagnostics == 0 then return end
   for _, v in ipairs(diagnostics) do
-    print(string.format('Diagnostic description: %s', v.codeDescription.href))
+    print(string.format('Diagnostic description: %s', v.user_data.lsp.codeDescription.href))
   end
 end
 
