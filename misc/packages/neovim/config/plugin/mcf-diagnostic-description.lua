21c2adee3c7975d83dceb98f6675a33ab8ffbf46
Sergey Alexandrov <alexandrov88@gmail.com>
2024-11-19 16:32:54 +0100

    Neovim: make DiagnosticDescription more robust

--- misc/packages/neovim/config/plugin/mcf-diagnostic-description.lua
+++ misc/packages/neovim/config/plugin/mcf-diagnostic-description.lua
@@ -2,7 +2,9 @@ function _G.show_diagnostic_description()
   local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
   if #diagnostics == 0 then return end
   for _, v in ipairs(diagnostics) do
-    print(string.format('Diagnostic description: %s', v.user_data.lsp.codeDescription.href))
+    local description = 'no description available'
+    if v.user_data.lsp.codeDescription then description = v.user_data.lsp.codeDescription.href end
+    print(string.format('Diagnostic description: %s', description))
   end
 end
 
