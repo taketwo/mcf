a35fd3f1e945859b838dadf44260bb58c0082059
Sergey Alexandrov <alexandrov88@gmail.com>
2024-11-25 14:52:03 +0100

    Neovim: customize commit editor in Neogit

--- misc/packages/neovim/config/lua/plugins/neogit.lua
+++ misc/packages/neovim/config/lua/plugins/neogit.lua
@@ -10,7 +10,18 @@ return {
   opts = {
     graph_style = 'unicode',
     kind = 'vsplit',
+    commit_editor = {
+      staged_diff_split_kind = 'vsplit',
+    },
     mappings = {
+      commit_editor = {
+        ['<c-n>'] = 'NextMessage',
+        ['<c-h>'] = 'PrevMessage',
+        ['<c-r>'] = 'ResetMessage',
+        ['<m-n>'] = false,
+        ['<m-p>'] = false,
+        ['<m-r>'] = false,
+      },
       popup = {
         -- Reassign c/t to uppercase to avoid conflicts with navigation keymaps
         ['C'] = 'CommitPopup',
