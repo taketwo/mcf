03b725a225c26929296dcd9f05214b2ae6c91336
Sergey Alexandrov <alexandrov88@gmail.com>
2025-01-14 09:35:16 +0100

    Neovim: update DAP keymaps to be compatible with current WhichKey version

--- misc/packages/neovim/config/lua/mcf/config/dap/keymaps.lua
+++ misc/packages/neovim/config/lua/mcf/config/dap/keymaps.lua
@@ -8,44 +8,40 @@ function M.setup()
   -- local widgets = require('dap.ui.widgets')
   local wk = require('which-key')
   local keymap = {
-    d = {
-      name = 'Debug',
-      R = { dap.run_to_cursor, 'Run to cursor' },
-      E = {
-        function() dapui.eval(vim.fn.input({ prompt = '[Expression] > ' })) end,
-        'Evaluate input',
-      },
-      C = {
-        function() dap.set_breakpoint(vim.fn.input({ prompt = '[Condition] > ' })) end,
-        'Conditional breakpoint',
-      },
-      L = {
-        function() dap.set_breakpoint(nil, nil, vim.fn.input({ prompt = '[Message] > ' })) end,
-        'Logpoint',
-      },
-      U = { dapui.toggle, 'Toggle UI' },
-      b = { dap.step_back, 'Step back' },
-      c = { dap.continue, 'Continue' },
-      d = { dap.disconnect, 'Disconnect' },
-      e = { dapui.eval, 'Evaluate' },
-      g = { dap.session, 'Get session' },
-      -- TODO: Does not work
-      -- h = { widgets.hover, 'Hover variables' },
-      -- S = { widgets.scopes, 'Scopes' },
-      p = { dap.pause, 'Pause' },
-      q = { dap.close, 'Quit' },
-      r = { dap.repl.toggle, 'Toggle REPL' },
-      t = { dap.toggle_breakpoint, 'Toggle breakpoint' },
-      x = { dap.terminate, 'Terminate' },
-      u = { dap.step_out, 'Step out' },
-      i = { dap.step_into, 'Step into' },
-      o = { dap.step_over, 'Step over' },
+    { '<Leader>d', group = 'Debug' },
+    { '<Leader>dR', dap.run_to_cursor, desc = 'Run to cursor' },
+    { '<Leader>dE', function() dapui.eval(vim.fn.input({ prompt = '[Expression] > ' })) end, desc = 'Evaluate input' },
+    {
+      '<Leader>dC',
+      function() dap.set_breakpoint(vim.fn.input({ prompt = '[Condition] > ' })) end,
+      desc = 'Conditional breakpoint',
     },
+    {
+      '<Leader>dL',
+      function() dap.set_breakpoint(nil, nil, vim.fn.input({ prompt = '[Message] > ' })) end,
+      desc = 'Logpoint',
+    },
+    { '<Leader>dU', dapui.toggle, desc = 'Toggle UI' },
+    { '<Leader>db', dap.step_back, desc = 'Step back' },
+    { '<Leader>dc', dap.continue, desc = 'Continue' },
+    { '<Leader>dd', dap.disconnect, desc = 'Disconnect' },
+    { '<Leader>de', dapui.eval, desc = 'Evaluate' },
+    { '<Leader>dg', dap.session, desc = 'Get session' },
+    -- TODO: Does not work
+    -- h = { widgets.hover, 'Hover variables' },
+    -- S = { widgets.scopes, 'Scopes' },
+    { '<Leader>dp', dap.pause, desc = 'Pause' },
+    { '<Leader>dq', dap.close, desc = 'Quit' },
+    { '<Leader>dr', dap.repl.toggle, desc = 'Toggle REPL' },
+    { '<Leader>dt', dap.toggle_breakpoint, desc = 'Toggle breakpoint' },
+    { '<Leader>dx', dap.terminate, desc = 'Terminate' },
+    { '<Leader>du', dap.step_out, desc = 'Step out' },
+    { '<Leader>di', dap.step_into, desc = 'Step into' },
+    { '<Leader>do', dap.step_over, desc = 'Step over' },
   }
 
   wk.add(keymap, {
     mode = 'n',
-    prefix = '<Leader>',
     buffer = nil,
     silent = true,
     noremap = true,
@@ -53,14 +49,11 @@ function M.setup()
   })
 
   local keymap_v = {
-    d = {
-      name = 'Debug',
-      e = { dapui.eval, 'Evaluate' },
-    },
+    { '<Leader>d', group = 'Debug' },
+    { '<Leader>de', dapui.eval, desc = 'Evaluate' },
   }
   wk.add(keymap_v, {
     mode = 'v',
-    prefix = '<Leader>',
     buffer = nil,
     silent = true,
     noremap = true,
@@ -68,12 +61,12 @@ function M.setup()
   })
 
   wk.add({
-    ['<F5>'] = { dap.continue, 'Continue' },
-    ['<F9>'] = { dap.toggle_breakpoint, 'Toggle breakpoint' },
-    ['<F10>'] = { dap.step_over, 'Step over' },
-    ['<F11>'] = { dap.step_into, 'Step into' },
-    ['<S-F11>'] = { dap.step_out, 'Step out' },
-    ['<S-F5>'] = { dap.terminate, 'Stop' },
+    { '<F5>', dap.continue, desc = 'Continue' },
+    { '<F9>', dap.toggle_breakpoint, desc = 'Toggle breakpoint' },
+    { '<F10>', dap.step_over, desc = 'Step over' },
+    { '<F11>', dap.step_into, desc = 'Step into' },
+    -- { '<S-F11>', dap.step_out, desc = 'Step out' },
+    { '<S-F5>', dap.terminate, desc = 'Stop' },
   }, {
     mode = 'n',
     buffer = nil,
