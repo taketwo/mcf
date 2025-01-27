dc78f8ab4596a5a06e2b446f2586bd9bc3bde3a2
Sergey Alexandrov <alexandrov88@gmail.com>
2025-01-27 22:26:33 +0100

    Neovim: add 'commit_message' prompt and a temporary hotkey to invoke it

--- misc/packages/neovim/config/lua/plugins/codecompanion-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/codecompanion-nvim.lua
@@ -15,6 +15,12 @@ return {
       { '<Leader>A', '', desc = 'AI support with CodeCompanion', mode = { 'n', 'v' } },
       { '<Leader>A<Space>', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Toggle chat window', mode = { 'n', 'v' } },
       { '<Leader>Aa', '<cmd>CodeCompanionActions<cr>', desc = 'Actions', mode = { 'n', 'v' } },
+      {
+        '<Space>',
+        function() require('codecompanion').prompt('commit_message') end,
+        desc = 'Generate commit message',
+        ft = 'gitcommit',
+      },
     },
     opts = {
       adapters = {
@@ -44,6 +50,38 @@ return {
         },
         agent = {
           adapter = 'copilot',
+        },
+      },
+      prompt_library = {
+        ['Commit Message'] = {
+          strategy = 'inline',
+          description = 'Generate a commit message',
+          opts = {
+            short_name = 'commit_message',
+            auto_submit = true,
+            placement = 'before|false',
+          },
+          prompts = {
+            {
+              role = 'user',
+              content = function()
+                return string.format(
+                  [[You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
+
+```diff
+%s
+```
+
+Output only the commit message without any explanations and follow-up suggestions.
+]],
+                  vim.fn.system('git diff --no-ext-diff --staged')
+                )
+              end,
+              opts = {
+                contains_code = true,
+              },
+            },
+          },
         },
       },
     },
