392d447c7aff8e31b85a731b6c9203cf811e9ef0
Sergey Alexandrov <alexandrov88@gmail.com>
2025-02-16 20:26:09 +0100

    Neovim: improve highlight transparency handling for floating windows

--- misc/packages/neovim/config/lua/plugins/onedark-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/onedark-nvim.lua
@@ -7,16 +7,21 @@ return {
       style = 'warmer',
       transparent = true,
       highlights = {
-        Search = { fg = 'none', bg = 'none', fmt = 'bold,underline' },
+        -- Background transparency setup
+        -- We want Neovim windows to have the same background as Tmux panes that they are in. (This allows to easily
+        -- differentiate between focused and unfocused panes.) The `transparent` option handles this for the most part,
+        -- but the floating windows (such as Telescope or Snacks Picker) require additional configuration.
+        NormalFloat = { bg = 'none' },
+        FloatBorder = { bg = 'none' },
         -- The highlight below changes the background of inactive Neovim windows in the same way as it does with inactive
         -- Tmux panes. On one hand, this is useful to understand which window is active. On the other hand, it destroys the
         -- visual consistency of floating windows (such as Telescope). Therefore, it is commented out.
         -- NormalNC = { fg = 'none', bg = '$bg1' },
+        Search = { fg = 'none', bg = 'none', fmt = 'bold,underline' },
         CurSearch = { fg = 'none', bg = 'none', fmt = 'bold,underline' },
         IncSearch = { fg = 'none', bg = 'none', fmt = 'bold,underline,standout' },
         CursorLineNr = { fmt = 'bold' },
         YankedRegion = { fg = '$red' },
-        FloatBorder = { bg = 'none' },
         IlluminatedWordText = { fmt = 'bold', bg = 'none' },
         IlluminatedWordRead = { fmt = 'bold', bg = 'none' },
         IlluminatedWordWrite = { fmt = 'bold', bg = 'none' },
