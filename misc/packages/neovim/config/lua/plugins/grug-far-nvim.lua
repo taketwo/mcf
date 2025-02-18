dd39dcd879a3b5b74ab404db6148bf92dd6aac82
Sergey Alexandrov <alexandrov88@gmail.com>
2025-02-18 22:03:07 +0100

    Neovim: improve Grug-far config

--- misc/packages/neovim/config/lua/plugins/grug-far-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/grug-far-nvim.lua
@@ -6,7 +6,13 @@ return {
     ---@diagnostic disable: missing-fields
     opts = {
       headerMaxWidth = 80,
+      openTargetWindow = {
+        preferredLocation = 'right',
+      },
       keymaps = {
+        openNextLocation = { n = '>' },
+        openPrevLocation = { n = '<' },
+        openLocation = { n = 'o' },
         close = { n = 'q' },
       },
     },
