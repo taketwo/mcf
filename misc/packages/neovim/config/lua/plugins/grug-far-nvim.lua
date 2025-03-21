2bb0074a2af64d1cc816147cd5976a4708703657
Sergey Alexandrov <alexandrov88@gmail.com>
2025-03-21 09:25:48 +0100

    Neovim: remove custom keymaps to jump between GrugFar input fields
    This is now included in the plugin by default.

--- misc/packages/neovim/config/lua/plugins/grug-far-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/grug-far-nvim.lua
@@ -30,21 +30,6 @@ return {
         group = vim.api.nvim_create_augroup('grug-far-keymap', { clear = true }),
         pattern = { 'grug-far' },
         callback = function()
-          local function jump_field(direction)
-            local MIN_FIELD_LINE = 2
-            local MAX_FIELD_LINE = 6
-            local current_line = vim.api.nvim_win_get_cursor(0)[1]
-            local next_line = current_line + direction
-            if next_line > MAX_FIELD_LINE then
-              next_line = MIN_FIELD_LINE
-            elseif next_line < MIN_FIELD_LINE then
-              next_line = MAX_FIELD_LINE
-            end
-            vim.api.nvim_win_set_cursor(vim.fn.bufwinid(0), { next_line, 0 })
-          end
-          -- Jump between input fields with Tab and Shift-Tab
-          vim.keymap.set({ 'n', 'i' }, '<Tab>', function() jump_field(1) end, { buffer = true })
-          vim.keymap.set({ 'n', 'i' }, '<S-Tab>', function() jump_field(-1) end, { buffer = true })
           -- Leave insert mode and jump to first result with Enter
           vim.keymap.set({ 'i' }, '<Enter>', '<Esc>>', { buffer = true, remap = true })
           -- Open location and close Grug-far window with O
