db577e9eb97c11d196a24eaa2344002410c64041
Sergey Alexandrov <alexandrov88@gmail.com>
2025-04-22 11:11:55 +0200

    Neovim: add keybindings for diffview
    Add a set of keybindings under the '<Leader>d' prefix to make diffview.nvim functionality more accessible. This includes commands for opening diffs, viewing file history, and toggling panels.

--- misc/packages/neovim/config/lua/plugins/diffview-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/diffview-nvim.lua
@@ -2,6 +2,15 @@ return {
   {
     'sindrets/diffview.nvim',
     cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+    keys = {
+      { '<Leader>d', '', desc = 'Diffs' },
+      { '<Leader>dH', '<cmd>DiffviewFileHistory %<cr>', desc = 'Open diff view file history (current file)' },
+      { '<Leader>dO', '<cmd>DiffviewOpen %<cr>', desc = 'Open diff view against current index (current file)' },
+      { '<Leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Close diff view' },
+      { '<Leader>dh', '<cmd>DiffviewFileHistory<cr>', desc = 'Open diff view file history' },
+      { '<Leader>do', '<cmd>DiffviewOpen<cr>', desc = 'Open diff view against current index' },
+      { '<Leader>dp', '<cmd>DiffviewToggleFiles<cr>', desc = 'Toggle diff view files panel' },
+    },
     opts = {
       enhanced_diff_hl = true,
     },
