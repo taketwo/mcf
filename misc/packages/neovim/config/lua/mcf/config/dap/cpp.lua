2b6dfa183009c90346d3e6b93a91007eade4eb08
Sergey Alexandrov <alexandrov88@gmail.com>
2025-04-24 10:47:09 +0200

    Neovim: refactor ROS executable path handling in DAP config
    Improve the C++ debugger configuration:
    1. Extract the executable path construction to a separate function
    2. Handle cases where the derived executable doesn't exist by prompting the user
    3. Add a TODO note for overwriting existing configurations
    
    This makes the debugger setup more robust when the executable name doesn't match the file name.

--- misc/packages/neovim/config/lua/mcf/config/dap/cpp.lua
+++ misc/packages/neovim/config/lua/mcf/config/dap/cpp.lua
@@ -20,13 +20,14 @@ function M.setup()
       __call = function(config)
         local ros_package_name = vim.b.ros_package_name
         local ros_package_workspace = vim.b.ros_package_workspace
-        if ros_package_name and ros_package_workspace then
-          local executable_path = ros_package_workspace
-            .. '/devel/lib/'
-            .. ros_package_name
-            .. '/'
-            .. vim.fn.fnamemodify(vim.fn.expand('%'), ':t:r')
-          config.program = executable_path
+        local construct_executable_path = function(executable_name)
+          return ros_package_workspace .. '/devel/lib/' .. ros_package_name .. '/' .. executable_name
+        end
+        config.program = construct_executable_path(vim.fn.fnamemodify(vim.fn.expand('%'), ':t:r'))
+        if vim.fn.executable(config.program) == 0 then
+          config.program = construct_executable_path(vim.fn.input({
+            prompt = 'Executable: ',
+          }))
         end
         config.args = vim.split(
           vim.fn.input({
@@ -37,6 +38,7 @@ function M.setup()
         local config_copy = vim.deepcopy(config)
         setmetatable(config_copy, nil)
         config_copy.name = config_copy.name .. ' (again)'
+        -- TODO: Overwrite if already exists
         table.insert(dap.configurations.cpp, 1, config_copy)
         return config
       end,
