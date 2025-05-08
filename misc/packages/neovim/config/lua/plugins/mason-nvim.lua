7874761c493e97566a373dc0046a48d73109f915
Sergey Alexandrov <alexandrov88@gmail.com>
2025-05-08 21:57:38 +0200

    Neovim: refactor Mason plugins dependencies
    Simplify the mason.nvim plugin configuration and move the mason-lspconfig.nvim dependency under Mason.

--- misc/packages/neovim/config/lua/plugins/mason-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/mason-nvim.lua
@@ -1,24 +1,12 @@
 return {
   {
     'mason-org/mason.nvim',
+    dependencies = {
+      -- Config will be called from mcf.config.lsp.setup() after Mason and lspconfig are loaded
+      { 'mason-org/mason-lspconfig.nvim', config = false },
+    },
     cmd = { 'Mason', 'MasonUpdate' },
     build = ':MasonUpdate',
-    config = function(_, opts)
-      require('mason').setup(opts)
-
-      -- Setup handler for successful installation of packages.
-      -- It triggers FileType event to possibly load this newly installed LSP server (copied from LazyVim)
-      require('mason-registry'):on('package:install:success', function()
-        vim.defer_fn(
-          function()
-            require('lazy.core.handler.event').trigger({
-              event = 'FileType',
-              buf = vim.api.nvim_get_current_buf(),
-            })
-          end,
-          100
-        )
-      end)
-    end,
+    opts = {},
   },
 }
--- misc/packages/neovim/config/lua/plugins/nvim-lspconfig.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,6 @@ return {
     dependencies = {
       { 'folke/neoconf.nvim', cmd = 'Neoconf', config = false, dependencies = { 'nvim-lspconfig' } },
       'mason.nvim',
-      { 'mason-org/mason-lspconfig.nvim', config = false },
       'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
