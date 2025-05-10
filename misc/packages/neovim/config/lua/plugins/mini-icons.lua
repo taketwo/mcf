f3dbca0f746398611e3981d56fe7462d30fef474
Sergey Alexandrov <alexandrov88@gmail.com>
2025-05-10 18:46:37 +0200

    Neovim: configure custom LSP glyphs for mini.icons

--- misc/packages/neovim/config/lua/plugins/mini-icons.lua
+++ misc/packages/neovim/config/lua/plugins/mini-icons.lua
@@ -2,7 +2,35 @@ return {
   {
     'echasnovski/mini.icons',
     lazy = true,
-    opts = {},
+    opts = {
+      lsp = {
+        ['class'] = { glyph = '󰆧' },
+        ['color'] = { glyph = '󰏘' },
+        ['constant'] = { glyph = '󰏿' },
+        ['constructor'] = { glyph = '' },
+        ['enum'] = { glyph = '' },
+        ['enum_member'] = { glyph = '' },
+        ['event'] = { glyph = '' },
+        ['field'] = { glyph = '󰜢' },
+        ['file'] = { glyph = '󰈙' },
+        ['folder'] = { glyph = '' },
+        ['function'] = { glyph = '󰊕' },
+        ['interface'] = { glyph = '' },
+        ['keyword'] = { glyph = '󰌋' },
+        ['method'] = { glyph = '󰊕' },
+        ['module'] = { glyph = '󰕳' },
+        ['operator'] = { glyph = '󰆕' },
+        ['property'] = { glyph = '󰜢' },
+        ['reference'] = { glyph = '󰈇' },
+        ['snippet'] = { glyph = '' },
+        ['struct'] = { glyph = '󰙅' },
+        ['text'] = { glyph = '' },
+        ['type_parameter'] = { glyph = '󰊄' },
+        ['unit'] = { glyph = '󰑭' },
+        ['value'] = { glyph = '󰎠' },
+        ['variable'] = { glyph = '󰀫' },
+      },
+    },
     init = function()
       package.preload['nvim-web-devicons'] = function()
         require('mini.icons').mock_nvim_web_devicons()
