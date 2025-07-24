60c20526715eba50e6bd839475a2c3da5f6ced0c
Sergey Alexandrov <alexandrov88@gmail.com>
2025-07-24 13:08:10 +0200

    MCF: fix dircolors bash file location

--- misc/packages/20-dircolors.bash
+++
@@ -1,6 +0,0 @@
-# Install dircolors
-test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors/dircolors.one-dark)"
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
--- misc/packages/dircolors/dircolors.bash
+++ misc/packages/dircolors/dircolors.bash
@@ -1,1 +1,6 @@
-../20-dircolors.bash
+# Install dircolors
+test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors/dircolors.one-dark)"
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
