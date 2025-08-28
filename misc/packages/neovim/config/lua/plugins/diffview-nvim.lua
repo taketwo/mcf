86cede1bce23f4e0f1f7a2bc81782c0070e20f65
Sergey Alexandrov <alexandrov88@gmail.com>
2025-08-28 15:56:25 +0200

    Neovim: add diffview keybindings for comparing against master

--- misc/packages/neovim/config/lua/plugins/diffview-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/diffview-nvim.lua
@@ -5,9 +5,11 @@ return {
     keys = {
       { '<Leader>d', '', desc = 'Diffs' },
       { '<Leader>dH', '<cmd>DiffviewFileHistory %<cr>', desc = 'Open diff view file history (current file)' },
-      { '<Leader>dO', '<cmd>DiffviewOpen %<cr>', desc = 'Open diff view against current index (current file)' },
+      { '<Leader>dM', '<cmd>DiffviewOpen master -- %<cr>', desc = 'Open diff view against master (current file)' },
+      { '<Leader>dO', '<cmd>DiffviewOpen -- %<cr>', desc = 'Open diff view against current index (current file)' },
       { '<Leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Close diff view' },
       { '<Leader>dh', '<cmd>DiffviewFileHistory<cr>', desc = 'Open diff view file history' },
+      { '<Leader>dm', '<cmd>DiffviewOpen master<cr>', desc = 'Open diff view against master' },
       { '<Leader>do', '<cmd>DiffviewOpen<cr>', desc = 'Open diff view against current index' },
       { '<Leader>dp', '<cmd>DiffviewToggleFiles<cr>', desc = 'Toggle diff view files panel' },
     },
