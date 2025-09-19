fbc6bde6725fc4a2bfe66f760aa2a6e451cf4a24
Sergey Alexandrov <alexandrov88@gmail.com>
2025-09-19 12:00:27 +0200

    Clangd: clarify what goes where

--- misc/packages/neovim/config/after/lsp/clangd.lua
+++ misc/packages/neovim/config/after/lsp/clangd.lua
@@ -10,6 +10,8 @@ return {
     offsetEncoding = 'utf-16',
   },
   -- Some of the supported options are listed at: https://clangd.llvm.org/extensions
+  -- Note that these are LSP options, not clangd options. The latter are configured
+  -- in the global clangd configuration file.
   init_options = {
     usePlaceholders = true, -- TODO: Understand whether this is useful
     completeUnimported = true, -- TODO: Understand whether this is useful
