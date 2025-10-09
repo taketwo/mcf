5ebfa03ae6e1627584df4a4d7fa8083d0c8ece1b
Sergey Alexandrov <alexandrov88@gmail.com>
2025-10-09 07:33:46 +0200

    Neovim: add mermaid-playground plugin

---
+++ misc/packages/neovim/config/lua/plugins/mermaid-playground-nvim.lua
@@ -0,0 +1,23 @@
+return {
+  'selimacerbas/mermaid-playground.nvim',
+  dependencies = {
+    {
+      'barrett-ruth/live-server.nvim',
+      cmd = { 'LiveServerStart', 'LiveServerStop' },
+      config = true,
+    },
+  },
+  ft = { 'markdown' },
+  config = function()
+    require('mermaid_playground').setup({
+      workspace_dir = nil,
+      index_name = 'index.html',
+      diagram_name = 'diagram.mmd',
+      overwrite_index_on_start = false,
+      auto_refresh = true,
+      auto_refresh_events = { 'InsertLeave', 'TextChanged', 'TextChangedI', 'BufWritePost' },
+      debounce_ms = 450,
+      notify_on_refresh = false,
+    })
+  end,
+}
