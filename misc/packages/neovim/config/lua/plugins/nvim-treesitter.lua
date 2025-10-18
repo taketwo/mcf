24d0a8cf8ca24bd8d572d3f8bf9849802c278f58
Sergey Alexandrov <alexandrov88@gmail.com>
2025-10-18 20:41:51 +0200

    Neovim: attach textobject keymaps to existing buffers on load

--- misc/packages/neovim/config/lua/plugins/nvim-treesitter.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-treesitter.lua
@@ -14,60 +14,67 @@ return {
             return
           end
           TS.setup(opts)
-          vim.api.nvim_create_autocmd('FileType', {
-            group = vim.api.nvim_create_augroup('lazyvim_treesitter_textobjects', { clear = true }),
-            callback = function(ev)
-              if not LazyVim.treesitter.have(ev.match, 'textobjects') then return end
-              local moves = {
-                goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']p'] = '@parameter.inner' },
-                goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']P'] = '@parameter.inner' },
-                goto_previous_start = {
-                  ['[f'] = '@function.outer',
-                  ['[c'] = '@class.outer',
-                  ['[p'] = '@parameter.inner',
-                },
-                goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[P'] = '@parameter.inner' },
-              }
-              for method, keymaps in pairs(moves) do
-                for key, query in pairs(keymaps) do
-                  local desc = query:gsub('@', ''):gsub('%..*', '')
-                  desc = (key:sub(1, 1) == '[' and 'Previous ' or 'Next ') .. desc
-                  desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and ' end' or ' start')
-                  --Only set the keymap if we are not in diff mode with a c/C key
-                  if not (vim.wo.diff and key:find('[cC]')) then
-                    vim.keymap.set(
-                      { 'n', 'x', 'o' },
-                      key,
-                      function() require('nvim-treesitter-textobjects.move')[method](query, 'textobjects') end,
-                      { desc = desc, buffer = ev.buf, silent = true }
-                    )
-                  end
-                end
-              end
-              local swaps = {
-                swap_next = {
-                  ['<Leader>sn'] = { query = '@parameter.inner', desc = 'Shift parameter right' },
-                  ['<Leader>st'] = { query = '@function.outer', desc = 'Shift function down' },
-                  ['<Leader>sT'] = { query = '@class.outer', desc = 'Shift class down' },
-                },
-                swap_previous = {
-                  ['<Leader>sh'] = { query = '@parameter.inner', desc = 'Shift parameter left' },
-                  ['<Leader>sc'] = { query = '@function.outer', desc = 'Shift function up' },
-                  ['<Leader>sC'] = { query = '@class.outer', desc = 'Shift class up' },
-                },
-              }
-              for method, keymaps in pairs(swaps) do
-                for key, qd in pairs(keymaps) do
+
+          local function attach(buf)
+            local ft = vim.bo[buf].filetype
+            if not LazyVim.treesitter.have(ft, 'textobjects') then return end
+
+            local moves = {
+              goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']p'] = '@parameter.inner' },
+              goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']P'] = '@parameter.inner' },
+              goto_previous_start = {
+                ['[f'] = '@function.outer',
+                ['[c'] = '@class.outer',
+                ['[p'] = '@parameter.inner',
+              },
+              goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[P'] = '@parameter.inner' },
+            }
+            for method, keymaps in pairs(moves) do
+              for key, query in pairs(keymaps) do
+                local desc = query:gsub('@', ''):gsub('%..*', '')
+                desc = (key:sub(1, 1) == '[' and 'Previous ' or 'Next ') .. desc
+                desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and ' end' or ' start')
+                --Only set the keymap if we are not in diff mode with a c/C key
+                if not (vim.wo.diff and key:find('[cC]')) then
                   vim.keymap.set(
-                    { 'n', 'x' },
+                    { 'n', 'x', 'o' },
                     key,
-                    function() require('nvim-treesitter-textobjects.swap')[method](qd['query']) end,
-                    { desc = qd['desc'], buffer = ev.buf, silent = true }
+                    function() require('nvim-treesitter-textobjects.move')[method](query, 'textobjects') end,
+                    { desc = desc, buffer = buf, silent = true }
                   )
                 end
               end
-            end,
+            end
+            local swaps = {
+              swap_next = {
+                ['<Leader>sn'] = { query = '@parameter.inner', desc = 'Shift parameter right' },
+                ['<Leader>st'] = { query = '@function.outer', desc = 'Shift function down' },
+                ['<Leader>sT'] = { query = '@class.outer', desc = 'Shift class down' },
+              },
+              swap_previous = {
+                ['<Leader>sh'] = { query = '@parameter.inner', desc = 'Shift parameter left' },
+                ['<Leader>sc'] = { query = '@function.outer', desc = 'Shift function up' },
+                ['<Leader>sC'] = { query = '@class.outer', desc = 'Shift class up' },
+              },
+            }
+            for method, keymaps in pairs(swaps) do
+              for key, qd in pairs(keymaps) do
+                vim.keymap.set(
+                  { 'n', 'x' },
+                  key,
+                  function() require('nvim-treesitter-textobjects.swap')[method](qd['query']) end,
+                  { desc = qd['desc'], buffer = buf, silent = true }
+                )
+              end
+            end
+          end
+
+          vim.api.nvim_create_autocmd('FileType', {
+            group = vim.api.nvim_create_augroup('lazyvim_treesitter_textobjects', { clear = true }),
+            callback = function(ev) attach(ev.buf) end,
           })
+
+          vim.tbl_map(attach, vim.api.nvim_list_bufs())
         end,
       },
       {
@@ -98,7 +105,6 @@ return {
       end
       LazyVim.treesitter.build(function() TS.update(nil, { summary = true }) end)
     end,
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     event = { 'BufReadPost', 'BufNewFile' },
     cmd = { 'TSUpdate', 'TSUpdateSync', 'TSInstall', 'TSLog', 'TSUninstall' },
     ---@class lazyvim.TSConfig: TSConfig
