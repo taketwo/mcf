4c180a09cd42418d704ada137203781aed1d986d
Sergey Alexandrov <alexandrov88@gmail.com>
2025-10-27 22:07:34 +0100

    Neovim: simplify Sidekick lualine segment

--- misc/packages/neovim/config/lua/plugins/sidekick-nvim.lua
+++ misc/packages/neovim/config/lua/plugins/sidekick-nvim.lua
@@ -6,24 +6,14 @@ return {
         'nvim-lualine/lualine.nvim',
         event = 'VeryLazy',
         opts = function(_, opts)
-          local icons = {
-            Error = { ' ', 'DiagnosticError' },
-            Inactive = { ' ', 'MsgArea' },
-            Warning = { ' ', 'DiagnosticWarn' },
-            Normal = { ' ', 'Special' },
-          }
           -- TODO: Unify with copilot lualine status and possibly move to lualine plugin config
           table.insert(opts.sections.lualine_x, 2, {
             function()
-              local status = require('sidekick.status').get()
-              return status and vim.tbl_get(icons, status.kind, 1)
+              local status = require('sidekick.status').cli()
+              return ' ' .. (#status > 1 and #status or '')
             end,
-            cond = function() return require('sidekick.status').get() ~= nil end,
-            color = function()
-              local status = require('sidekick.status').get()
-              local hl = status and (status.busy and 'DiagnosticWarn' or vim.tbl_get(icons, status.kind, 2))
-              return { fg = Snacks.util.color(hl) }
-            end,
+            cond = function() return #require('sidekick.status').cli() > 0 end,
+            color = function() return { fg = Snacks.util.color('Debug') } end,
           })
         end,
       },
