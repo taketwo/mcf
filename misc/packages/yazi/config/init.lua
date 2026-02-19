2025aca8e59c6650e661a672b9cebc30f2caae18
Sergey Alexandrov <alexandrov88@gmail.com>
2026-02-19 10:43:32 +0100

    Yazi: add yaziline plugin

---
+++ misc/packages/yazi/config/init.lua
@@ -0,0 +1,14 @@
+require('yaziline'):setup({
+  color = '#98c379',
+  secondary_color = 'darkgray',
+  default_files_color = 'darkgray',
+  selected_files_color = 'blue',
+  yanked_files_color = 'white',
+  cut_files_color = 'red',
+  separator_style = 'angly',
+  select_symbol = '',
+  yank_symbol = '󰆐',
+  filename_max_length = 24,
+  filename_truncate_length = 6,
+  filename_truncate_separator = '...',
+})
