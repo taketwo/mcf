b67eaf56355da1d0bf15f7519d3a3a781c7d0327
Sergey Alexandrov <alexandrov88@gmail.com>
2026-02-25 11:21:25 +0100

    Yazi: remove color settings that duplicate defaults

--- misc/packages/yazi/config/init.lua
+++ misc/packages/yazi/config/init.lua
@@ -1,10 +1,6 @@
 require('yaziline'):setup({
   color = '#98c379',
   secondary_color = 'darkgray',
-  default_files_color = 'darkgray',
-  selected_files_color = 'blue',
-  yanked_files_color = 'white',
-  cut_files_color = 'red',
   separator_style = 'angly',
   select_symbol = '',
   yank_symbol = '󰆐',
