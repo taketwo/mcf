99e817bab8e712e1b96953274dce079148d322e8
Sergey Alexandrov <alexandrov88@gmail.com>
2026-04-30 16:31:03 +0300

    Neovim: update nvim-ros keymaps that use pickers

--- misc/packages/neovim/config/lua/plugins/nvim-ros.lua
+++ misc/packages/neovim/config/lua/plugins/nvim-ros.lua
@@ -2,15 +2,15 @@ return {
   {
     'taketwo/nvim-ros',
     dependencies = {
-      { 'nvim-telescope/telescope.nvim' },
+      { 'folke/snacks.nvim' },
       { 'taketwo/vim-ros' },
     },
     keys = {
       { '<Leader>r', '', desc = 'ROS' },
-      { '<Leader>re', '<cmd>Telescope ros ed<cr>', desc = 'Open file in package' },
-      { '<Leader>rm', '<cmd>Telescope ros msg<cr>', desc = 'Open message' },
-      { '<Leader>rs', '<cmd>Telescope ros srv<cr>', desc = 'Open service' },
-      { '<Leader>ra', '<cmd>Telescope ros action<cr>', desc = 'Open action' },
+      { '<Leader>rf', function() require('nvim-ros.snacks.pickers').file_picker() end, desc = 'Open file' },
+      { '<Leader>rm', function() require('nvim-ros.snacks.pickers').msg_picker() end, desc = 'Open message' },
+      { '<Leader>rs', function() require('nvim-ros.snacks.pickers').srv_picker() end, desc = 'Open service' },
+      { '<Leader>ra', function() require('nvim-ros.snacks.pickers').action_picker() end, desc = 'Open action' },
     },
     ft = { 'rosmsg', 'rossrv', 'rosaction' },
     opts = {
